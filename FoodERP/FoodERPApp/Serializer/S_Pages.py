d634f66fc8976f68415e4bbe44ba5c38a470700b
attribsolutions <87567521+pradnyaubale@users.noreply.github.com>
2022-06-13 16:16:39 +0530

    S_Pages Updates
    RelatedPageID,
    RelatedPageName

--- FoodERP/FoodERPApp/Serializer/S_Pages.py
+++ FoodERP/FoodERPApp/Serializer/S_Pages.py
@@ -4,12 +4,20 @@ from ..Serializer.S_RoleAccess import DynamicFieldsModelSerializer
 
 from ..models import *
 
-class M_PagesSerializer(DynamicFieldsModelSerializer):
-    Module = H_ModulesSerializer()
-    # SubModule = H_SubModulesSerializer()
-    class Meta:
-        model = M_Pages 
-        fields ="__all__"
+class M_PagesSerializer(serializers.Serializer):
+    ID = serializers.IntegerField(read_only=True)
+    Name = serializers.CharField(max_length=100)
+    Description = serializers.CharField(max_length=100)
+    ModuleID = serializers.IntegerField(read_only=True)
+    ModuleName=serializers.CharField(max_length=100)
+    isActive = serializers.BooleanField(default=False)
+    DisplayIndex = serializers.IntegerField()
+    Icon = serializers.CharField(max_length=100)
+    ActualPagePath = serializers.CharField(max_length=100)
+    isShowOnMenu = serializers.BooleanField(default=False)
+    PageType = serializers.IntegerField()
+    RelatedPageID = serializers.IntegerField()
+    RelatedPageName=serializers.CharField(max_length=100)   
 
 class M_PagesSerializer1(serializers.ModelSerializer):
     # Module = H_ModulesSerializer()
