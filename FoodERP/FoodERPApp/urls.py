a41ce0a0f905b2cdfbaee386711dfaa1ba08ff2b
SiddhantPanchwatkar <105641853+SiddhantPanchwatkar@users.noreply.github.com>
2022-06-17 12:34:47 +0530

    M_ItemsGroup Module
    M_ItemsGroup Modules all Method run sucesssfully

--- FoodERP/FoodERPApp/urls.py
+++ FoodERP/FoodERPApp/urls.py
@@ -24,6 +24,9 @@ from .Views.V_Items import *
 
 from .Views.V_Employees import *
 
+from .Views.V_ItemsGroup import *
+
+
 
 
 
@@ -74,5 +77,8 @@ urlpatterns = [
     url(r'M_Employees/([0-9]+)', M_EmployeesViewSecond.as_view()),
     url(r'M_Employees', M_EmployeesView.as_view()),
   
+
+    url(r'ItemsGroup/([0-9]+)', M_ItemsGroupViewSecond.as_view()),
+    url(r'ItemsGroup', M_ItemsGroupView.as_view()),
      
 ]
