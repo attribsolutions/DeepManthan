958501d81f155d32cca8bcfc95eb50551124fe3e
SiddhantPanchwatkar <105641853+SiddhantPanchwatkar@users.noreply.github.com>
2022-06-20 10:03:19 +0530

    M_Employee
    in M_Employee table add fields

--- FoodERP/FoodERPApp/Serializer/S_Employees.py
+++ FoodERP/FoodERPApp/Serializer/S_Employees.py
@@ -1,8 +1,13 @@
 from dataclasses import fields
 from ..models import *
 from rest_framework import serializers
+from FoodERPApp.Serializer.S_State import *
+from FoodERPApp.Serializer.S_Resion import *
+
 
 class M_EmployeesSerializer(serializers.ModelSerializer):
     class Meta:
         model = M_Employees
         fields = '__all__'
+ 
+ 
