122fc061b2067f4298156753aad1980ce20dc74f
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2022-06-29 12:01:56 +0530

    Update models.py

--- FoodERP/FoodERPApp/models.py
+++ FoodERP/FoodERPApp/models.py
@@ -152,9 +152,7 @@ class M_Parties(models.Model):
     companyID =  models.IntegerField()
     CustomerDivision =  models.IntegerField()
     Email = models.EmailField(max_length=200)
-    MobileNo = models.IntegerField(null=False, blank=False, unique=True,
-    validators=[MinValueValidator(12),
-                MaxValueValidator(12)])
+    MobileNo = models.BigIntegerField(null=False, blank=False, unique=True)
     Address = models.CharField(max_length=500)
     PIN = models.CharField(max_length=500)
     State = models.IntegerField()
