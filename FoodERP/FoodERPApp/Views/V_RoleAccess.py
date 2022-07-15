0c20d0eb7ea9811b8d7712d097409d536bfd024e
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2022-07-15 15:35:40 +0530

    RoleAccess

--- FoodERP/FoodERPApp/Views/V_RoleAccess.py
+++ FoodERP/FoodERPApp/Views/V_RoleAccess.py
@@ -16,14 +16,13 @@ class RoleAccessView(RetrieveAPIView):
     permission_classes = (IsAuthenticated,)
     authentication_class = JSONWebTokenAuthentication
     
-    def get(self, request,id=0):
-        
+    def get(self, request,Role=0,Division=0,Company=0):
         modules = M_RoleAccess.objects.raw(
             '''SELECT distinct Modules_id id ,h_modules.id, h_modules.Name,h_modules.DisplayIndex 
 FROM m_roleaccess 
 join h_modules on h_modules.id=m_roleaccess.Modules_id
-where Role_id =1 AND M_RoleAccess.Company_id=1 AND M_RoleAccess.Division_id=1 
-ORDER BY h_modules.DisplayIndex''')
+where Role_id =%s AND M_RoleAccess.Division_id=%s AND M_RoleAccess.Company_id=%s
+ORDER BY h_modules.DisplayIndex''',([Role],[Division],[Company]))
         data = M_RoleAccessSerializerfordistinctModule(modules, many=True).data
         Moduledata = list()
         for a in data:
@@ -33,7 +32,7 @@ m_pages.Icon,m_pages.isActive,m_pages.isShowOnMenu,m_pages.Module_id,
 m_pages.PageType,m_pages.RelatedPageID, 
 Pages_id FROM erpdatabase.m_roleaccess
 JOIN m_pages ON m_pages.id=m_roleaccess.Pages_id 
-WHERE Role_id=1 AND  Modules_id=%s ''',[id])
+WHERE Role_id=%s AND  Modules_id=%s ''',([Role],[id]))
 
            
             PageSerializer = M_PagesSerializerforRoleAccessNEW(query,  many=True).data
@@ -88,66 +87,6 @@ WHERE Role_id=1 AND  Modules_id=%s ''',[id])
         except Exception as e:
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  Exception(e), 'Data': []})
 
-
-class RoleAccessViewSecond(CreateAPIView):
-    
-    permission_classes = (IsAuthenticated,)
-    authentication__Class = JSONWebTokenAuthentication
-    
-    
-    def get(self, request,Role=0,Division=0,Company=0):
-    
-        modules = M_RoleAccess.objects.raw(
-            '''SELECT distinct Modules_id id ,h_modules.id, h_modules.Name,h_modules.DisplayIndex 
-FROM m_roleaccess 
-join h_modules on h_modules.id=m_roleaccess.Modules_id
-where Role_id =%s AND M_RoleAccess.Division_id=%s AND M_RoleAccess.Company_id=%s
-ORDER BY h_modules.DisplayIndex''',([Role],[Division],[Company]))
-        data = M_RoleAccessSerializerfordistinctModule(modules, many=True).data
-        Moduledata = list()
-        for a in data:
-            id=a['id']
-            query=M_RoleAccess.objects.raw('''SELECT m_roleaccess.id,m_pages.Name,m_pages.Description,m_pages.ActualPagePath,m_pages.DisplayIndex,
-m_pages.Icon,m_pages.isActive,m_pages.isShowOnMenu,m_pages.Module_id,
-m_pages.PageType,m_pages.RelatedPageID, 
-Pages_id FROM erpdatabase.m_roleaccess
-JOIN m_pages ON m_pages.id=m_roleaccess.Pages_id 
-WHERE Role_id=%s AND  Modules_id=%s ''',([Role],[id]))
-
-           
-            PageSerializer = M_PagesSerializerforRoleAccessNEW(query,  many=True).data
-            Pagesdata = list()
-            for a1 in PageSerializer:
-                id=a1['id']
-                RolePageAccess=MC_RolePageAccess.objects.raw(''' SELECT mc_rolepageaccess.PageAccess_id id,Name  FROM mc_rolepageaccess 
-                JOIN h_pageaccess ON h_pageaccess.id=mc_rolepageaccess.PageAccess_id  WHERE mc_rolepageaccess.RoleAccess_id=%s ''',[id])
-                RolePageAccessSerializer = MC_RolePageAccessSerializer(RolePageAccess,  many=True).data
-                Pagesdata.append( {
-                    "id": a1['Pages_id'],
-                    "Name": a1['Name'],
-                    "DisplayIndex": a1['DisplayIndex'],
-                    "Icon": a1['Icon'],
-                    "ActualPagePath": a1['ActualPagePath'],
-                    "isShowOnMenu": a1['isShowOnMenu'], 
-                    "RolePageAccess" :RolePageAccessSerializer
-             })
-
-            response1 = {
-                "ModuleID": a['id'],
-                "ModuleName": a['Name'],
-                "ModuleData": Pagesdata,
-               
-            }
-            Moduledata.append(response1)
-
-        response = {
-            "StatusCode": 200,
-            "Status": True,
-            "Message": " ",
-            "Data": Moduledata,
-        }
-        return Response(response)
-    
     
 class RoleAccessViewList(RetrieveAPIView):
     
@@ -170,15 +109,4 @@ class RoleAccessViewList(RetrieveAPIView):
                         
             except Exception as e:
                 return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  Exception(e), 'Data': []})
-    
-    @transaction.atomic()
-    def delete(self, request, id=0):
-        try:
-            with transaction.atomic():
-                RoleAccessdata = M_RoleAccess.objects.get(id=id)
-                RoleAccessdata.delete()
-                return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'RoleAccess  Deleted Successfully', 'Data':[]})
-        except M_RoleAccess.DoesNotExist:
-            return JsonResponse({'StatusCode': 204, 'Status': True, 'Message':'RoleAccess Not available', 'Data': []})        
-
-                              
+ 
