3d187b7494932f845174621acf33fcf769ea3b9a
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2022-07-27 11:38:04 +0530

    Update V_RoleAccess.py
    Updated V_RoleAccess.py

--- FoodERP/FoodERPApp/Views/V_RoleAccess.py
+++ FoodERP/FoodERPApp/Views/V_RoleAccess.py
@@ -124,7 +124,7 @@ class RoleAccessViewNewUpdated(RetrieveAPIView):
     authentication_class = JSONWebTokenAuthentication
 
     def get(self, request,Division=0,Role=0):
-        roleaccessquery = M_RoleAccess.objects.raw('''SELECT m_roleaccess.id id, h_modules.id moduleid, h_modules.Name ModuleName,m_pages.id pageid,m_pages.RelatedPageID, m_pages.name PageName  FROM erpdatabase.m_roleaccess JOIN m_pages ON m_pages.id=m_roleaccess.Pages_id JOIN h_modules ON h_modules.id=m_roleaccess.Modules_id WHERE Division_id=%s AND Role_id=%s ''',([Division],[Role]))
+        roleaccessquery = M_RoleAccess.objects.raw('''SELECT m_roleaccess.id id, h_modules.id moduleid, h_modules.Name ModuleName,m_pages.id pageid,m_pages.RelatedPageID, m_pages.name PageName  FROM erpdatabase.m_roleaccess JOIN m_pages ON m_pages.id=m_roleaccess.Pages_id JOIN h_modules ON h_modules.id=m_roleaccess.Modules_id WHERE m_pages.PageType=2 AND Role_id=%s AND Division_id=%s   ''',([Role],[Division]))
         # return JsonResponse({'query':  str(roleaccessquery.query)})
         RoleAccessdata = M_RoleAccessSerializerNewUpdated(roleaccessquery, many=True).data
         # return JsonResponse({'data':  RoleAccessdata})
