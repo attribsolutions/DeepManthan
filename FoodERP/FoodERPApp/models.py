545dd142cd8a3cae587260beaaedb52fad2d7ed2
pradnyaubale <87567521+pradnyaubale@users.noreply.github.com>
2022-07-28 14:04:28 +0530

    Update models.py

--- FoodERP/FoodERPApp/models.py
+++ FoodERP/FoodERPApp/models.py
@@ -290,6 +290,7 @@ class M_Roles(models.Model):
     Name = models.CharField(max_length=100)
     Description = models.CharField(max_length=100)
     isActive = models.BooleanField(default=False)
+    isSCMRole = models.BooleanField(default=False)
     Dashboard = models.CharField(max_length=200)
     CreatedBy = models.IntegerField()
     CreatedOn = models.DateTimeField(auto_now_add=True)
@@ -306,6 +307,7 @@ class M_Roles(models.Model):
 class MC_UserRoles(models.Model):
 
     User = models.ForeignKey(M_Users, related_name='UserRole',  on_delete=models.CASCADE)
+    Role = models.ForeignKey(M_Parties,related_name='userparty',  on_delete=models.DO_NOTHING)
     Role = models.ForeignKey(M_Roles,related_name='Role',  on_delete=models.DO_NOTHING)
 
     class Meta:
