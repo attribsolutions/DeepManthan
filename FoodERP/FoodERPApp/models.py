60348121067c1fb1edddc95186e88f39e0ad0033
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2022-09-17 17:55:52 +0530

    models.py

--- FoodERP/FoodERPApp/models.py
+++ FoodERP/FoodERPApp/models.py
@@ -108,8 +108,8 @@ class M_AddressTypes(models.Model):
 class M_Parties(models.Model):
 
     Name = models.CharField(max_length=500)
-    PartyType = models.ForeignKey(
-        M_PartyType, related_name='PartyType', on_delete=models.DO_NOTHING, blank=True)
+    # PartyType = models.ForeignKey(
+    #     M_PartyType, related_name='PartyType', on_delete=models.DO_NOTHING, blank=True)
     DivisionType = models.ForeignKey(
         M_DivisionType, related_name='PartiesDivision', on_delete=models.DO_NOTHING)
     Company = models.ForeignKey(
