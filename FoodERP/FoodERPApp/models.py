8185301a21d8ca73fb861d25a9b6fe601a70bdec
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2022-09-21 10:06:30 +0530

    models.py

--- FoodERP/FoodERPApp/models.py
+++ FoodERP/FoodERPApp/models.py
@@ -573,7 +573,7 @@ class M_MRPMaster(models.Model):
     'Customer means M_Parties ID'
     Party =models.ForeignKey(M_Parties, related_name='MRPParty', on_delete=models.DO_NOTHING,null=True,blank=True)
     EffectiveDate = models.DateField()
-    Item = Item = models.ForeignKey(M_Items, related_name='ItemMRPDetails', on_delete=models.CASCADE)
+    Item = models.ForeignKey(M_Items, related_name='ItemMRPDetails', on_delete=models.CASCADE)
     MRP = models.DecimalField(max_digits=20, decimal_places=2)
     Company = models.ForeignKey(C_Companies, related_name='MRPCompany', on_delete=models.DO_NOTHING)
     CreatedBy = models.IntegerField()
