99a542fd84211ffc931981de0cfb78ce5a34c85b
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2022-10-20 11:24:49 +0530

    Models.py updates

--- FoodERP/FoodERPApp/models.py
+++ FoodERP/FoodERPApp/models.py
@@ -663,7 +663,7 @@ class TC_OrderItems(models.Model):
     MRP = models.ForeignKey(M_MRPMaster, related_name='OrderItemMRP', on_delete=models.DO_NOTHING,null=True,blank=True)
     Rate = models.DecimalField(max_digits=10, decimal_places=2)
     Unit = models.ForeignKey(MC_ItemUnits, related_name='OrderUnitID', on_delete=models.DO_NOTHING)
-    BaseUnitQuantity = models.DecimalField(max_digits=5, decimal_places=2)
+    BaseUnitQuantity = models.DecimalField(max_digits=5, decimal_places=3)
     GST = models.ForeignKey(M_GSTHSNCode, related_name='OrderItemGST', on_delete=models.DO_NOTHING)
     Margin = models.ForeignKey(M_MarginMaster, related_name='OrderItemMargin', on_delete=models.DO_NOTHING,null=True,blank=True)
     BasicAmount = models.DecimalField(max_digits=20, decimal_places=2)
