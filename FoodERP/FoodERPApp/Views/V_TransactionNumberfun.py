67d176420a7a1f7452ca5bd5c5a400f2409c3b9e
pradnyaubale <pradnyaubale12@gmail.com>
2022-11-11 14:41:37 +0530

    Update V_TransactionNumberfun.py

--- FoodERP/FoodERPApp/Views/V_TransactionNumberfun.py
+++ FoodERP/FoodERPApp/Views/V_TransactionNumberfun.py
@@ -6,12 +6,13 @@ from ..Serializer.S_Orders import *
 class GetMaxNumber:
 
     def GetOrderNumber(*args):
+        
         MaxOrderNumber=T_Orders.objects.filter(Division_id=args[0]).filter(OrderType=args[1]).values('OrderNo').order_by('-id')[:1]
-        a=int(MaxOrderNumber[0]['OrderNo'])
-      
-        if a is None:
+       
+        if not MaxOrderNumber :
             a=1
         else:
+            a=int(MaxOrderNumber[0]['OrderNo'])
             a=a+1
         
         return a
