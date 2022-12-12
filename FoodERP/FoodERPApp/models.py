2c9cd72423bc8e9466af50bc2df9e9ed70f4f83d
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2022-12-12 16:24:34 +0530

    model changes
    Unit

--- FoodERP/FoodERPApp/models.py
+++ FoodERP/FoodERPApp/models.py
@@ -945,6 +945,7 @@ class MC_BillOfMaterialItems(models.Model):
 class T_WorkOrder(models.Model):
     WorkOrderDate = models.DateField()
     Item = models.ForeignKey(M_Items, on_delete=models.PROTECT)
+    Unit = models.ForeignKey(MC_ItemUnits, related_name='WorkOrderUnitID', on_delete=models.PROTECT)
     Bom = models.ForeignKey(M_BillOfMaterial, related_name='BomID', on_delete=models.PROTECT)
     NumberOfLot = models.IntegerField()
     Quantity = models.DecimalField(max_digits=10, decimal_places=3)
@@ -963,7 +964,7 @@ class TC_WorkOrderItems(models.Model):
     Item = models.ForeignKey(M_Items, on_delete=models.PROTECT)
     BomQuantity = models.DecimalField(max_digits=10, decimal_places=3) 
     Quantity = models.DecimalField(max_digits=10, decimal_places=3) 
-    Unit = models.ForeignKey(MC_ItemUnits, related_name='WorkOrderUnitID', on_delete=models.PROTECT)
+    Unit = models.ForeignKey(MC_ItemUnits, related_name='WorkOrderItemUnitID', on_delete=models.PROTECT)
     
     class Meta:
         db_table = "TC_WorkOrderItems"   
