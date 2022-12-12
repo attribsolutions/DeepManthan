b72403c6cf5058a3b47f7f55014df35953c9b3df
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2022-12-12 15:53:51 +0530

    model

--- FoodERP/FoodERPApp/models.py
+++ FoodERP/FoodERPApp/models.py
@@ -968,34 +968,7 @@ class TC_WorkOrderItems(models.Model):
     Unit = models.ForeignKey(MC_ItemUnits, related_name='WorkOrderUnitID', on_delete=models.PROTECT)
     
     class Meta:
-        db_table = "TC_WorkOrderItems"
-
-class T_MaterialIssue(models.Model):
-    MaterialIssueDate = models.DateField()
-    Item = models.ForeignKey(M_Items, on_delete=models.PROTECT)
-    NumberOfLot = models.IntegerField()
-    LotQuantity = models.DecimalField(max_digits=10, decimal_places=3)
-    Status=models.IntegerField()
-    ReIssueID = models.IntegerField()
-    IsReIssueID = models.BooleanField(default=False)
-    Company = models.ForeignKey(C_Companies, on_delete=models.PROTECT)
-    Division = models.ForeignKey(M_Parties, on_delete=models.PROTECT)
-    CreatedBy = models.IntegerField()
-    CreatedOn = models.DateTimeField(auto_now_add=True)
-    UpdatedBy = models.IntegerField()
-    UpdatedOn = models.DateTimeField(auto_now=True)
-    
-    class Meta:
-        db_table = "T_MaterialIssue"
-
-
- 
-class TC_MaterialIssueWorkOrders(models.Model):
-    WorkOrder = models.ForeignKey(T_WorkOrder, related_name='abc', on_delete=models.CASCADE)
-    MaterialIssue = models.ForeignKey(T_MaterialIssue,  on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = "TC_MaterialIssueWorkOrders"        
+        db_table = "TC_WorkOrderItems"   
                     
          
 class T_DeliveryChallans(models.Model):
@@ -1010,6 +983,7 @@ class T_DeliveryChallans(models.Model):
     CreatedOn = models.DateTimeField(auto_now_add=True)
     UpdatedBy = models.IntegerField()
     UpdatedOn = models.DateTimeField(auto_now=True)
+
 
     class Meta:
         db_table = "T_DeliveryChallans"
