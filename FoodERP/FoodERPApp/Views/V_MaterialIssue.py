9bf7bfe6a73d5b14475e83a66b6abce41e7c9ee8
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2022-12-14 16:08:25 +0530

    Material Issue go button api

--- FoodERP/FoodERPApp/Views/V_MaterialIssue.py
+++ FoodERP/FoodERPApp/Views/V_MaterialIssue.py
@@ -6,8 +6,8 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from django.db import IntegrityError, connection, transaction
 from rest_framework.parsers import JSONParser
 from django.db.models import Sum
-from ..Serializer.S_Orders import *
 from ..Serializer.S_WorkOrder import *
+from ..Serializer.S_MaterialIssue import *
 from ..models import *
 
 class WorkOrderDetailsView(CreateAPIView):
@@ -23,47 +23,73 @@ class WorkOrderDetailsView(CreateAPIView):
                 WorkOrderID = WorkOrderDetailsdata['WorkOrder']
                 ItemID = WorkOrderDetailsdata['Item']
                 CompanyID = WorkOrderDetailsdata['Company']
-                DivisionID = WorkOrderDetailsdata['Division']
-                Query = T_WorkOrder.objects.filter(id=WorkOrderID,Item_id=ItemID,Company_id=CompanyID,Division_id=DivisionID)
-                return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': '', 'Data': str(Query.query)})
+                PartyID = WorkOrderDetailsdata['Party']
+                GetQuantity = WorkOrderDetailsdata['Quantity']
+                Query = T_WorkOrder.objects.filter(id=WorkOrderID,Item_id=ItemID,Company_id=CompanyID,Party_id=PartyID)
+                # return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': '', 'Data': str(Query.query)})
                 if Query.exists():
                     WorkOrder_Serializer = WorkOrderSerializerSecond(Query,many=True).data
                     WorkOrderData = list()
                     # return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': '', 'Data': WorkOrder_Serializer})
                     for a in WorkOrder_Serializer:
                         MaterialDetails =list()
-                        total=0
                         for b in a['WorkOrderItems']:
                             Item = b['Item']['id']
-                            obatchwisestockquery=O_BatchWiseLiveStock.objects.raw(''' SELECT O_BatchWiseLiveStock.id,O_BatchWiseLiveStock.Item_id,SUM(O_BatchWiseLiveStock.BaseUnitQuantity) AS actualStock FROM O_BatchWiseLiveStock WHERE O_BatchWiseLiveStock.Item_id = %s AND O_BatchWiseLiveStock.Party_id= GROUP BY O_BatchWiseLiveStock.Item_id''', [Item])
+                            obatchwisestockquery= O_BatchWiseLiveStock.objects.filter(Item_id=Item,Party_id=PartyID)
+                            # return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': '', 'Data': str(obatchwisestockquery.query)})
                             if not obatchwisestockquery:
-                                StockQty =0.0
+                                StockQtySerialize_data =[]
                             else:
-                                StockQtySerialize_data = StockQtyserializer(obatchwisestockquery, many=True).data
-                                StockQty = StockQtySerialize_data[0]['actualStock']
-                            Qty = float(b['Quantity']) /float(a['EstimatedOutputQty'])
-                            ActualQty = float(GetQuantity * Qty)
+                                StockQtySerialize_data = StockQtyserializerForMaterialIssue(obatchwisestockquery, many=True).data
+                                Qty = float(b['Quantity']) /float(GetQuantity)
+                                ActualQty = float(GetQuantity * Qty)
+                                stockDatalist = list()
+                                add=0
+                                amount = 0
+                                p =0
+                                for c in StockQtySerialize_data:
+                                    if amount==0:
+                                        if(float(c['BaseUnitQuantity']) > float(ActualQty)):
+                                            p=float(ActualQty)
+                                            add=float(add) + float(ActualQty)
+                                        else:
+                                            p=float(c['BaseUnitQuantity'])
+                                            amount = float(ActualQty)- float(c['BaseUnitQuantity'])
+                                            add = float(add) + float(c['BaseUnitQuantity'])
+                                    else:
+                                        if(float(amount) > float(c['BaseUnitQuantity'])):
+                                            p=float(c['BaseUnitQuantity'])
+                                            amount = float(amount)-float(c['BaseUnitQuantity'])
+                                            add = float(add) + float(c['BaseUnitQuantity'])
+                                        else:
+                                            p=float(amount)
+                                            add = float(add) + float(amount) 
+                                            amount= float(c['BaseUnitQuantity'])-float(amount)           
+                                  
+                                          
+                                    stockDatalist.append({
+                                        "id": c['id'],
+                                        "Item":c['Item'],
+                                        "BatchDate":c['BatchDate'],
+                                        "BatchCode":c['BatchCode'],
+                                        "SystemBatchDate":c['SystemBatchDate'],
+                                        "SystemBatchCode":c['SystemBatchCode'],
+                                        "ObatchwiseQuantity":c['Quantity'],
+                                        "BaseUnitQuantity":c['BaseUnitQuantity'],
+                                        "Qty":p
+                                    })
                             MaterialDetails.append({
                                 "id": b['id'],
                                 "Item":b['Item']['id'],
                                 "ItemName":b['Item']['Name'], 
                                 "Unit": b['Unit']['id'],
                                 "UnitName": b['Unit']['UnitID']['Name'],
-                                "StockQuantity":StockQty,
-                                "BomQuantity":b['Quantity'],
-                                "Quantity":ActualQty
+                                "Quantity":b['Quantity'],
+                                "BatchesData":stockDatalist   
                             })
-                        BillofmaterialData.append({
-                            "id": a['id'],
-                            "IsActive": a['IsActive'],
-                            "Item":a['Item']['id'],
-                            "ItemName":a['Item']['Name'],
-                            "Stock":Stock,
-                            "EstimatedOutputQty": round(GetQuantity, 2),  
-                            "Unit": a['Unit']['id'],
-                            "UnitName": a['Unit']['UnitID']['Name'],
-                            "BOMItems":MaterialDetails
-                        })
-                    return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': '', 'Data': BillofmaterialData[0]})
-        except M_BillOfMaterial.DoesNotExist:
+                        WorkOrderData.append({
+                            "ItemsData":MaterialDetails
+                        })    
+                    return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': '', 'Data':WorkOrderData[0]})
+        except T_WorkOrder.DoesNotExist:
             return JsonResponse({'StatusCode': 204, 'Status': True, 'Message': 'Bill Of Material Not available', 'Data': []})
