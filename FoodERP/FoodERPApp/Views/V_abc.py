53d9ba79a915e701b8e4b62b3ecdd6fea2eee68d
pradnyaubale <pradnyaubale12@gmail.com>
2023-01-13 10:59:31 +0530

    Update V_abc.py

--- FoodERP/FoodERPApp/Views/V_abc.py
+++ FoodERP/FoodERPApp/Views/V_abc.py
@@ -6,6 +6,8 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from django.db import IntegrityError, connection, transaction
 from rest_framework.parsers import JSONParser,MultiPartParser, FormParser
 
+from ..Views.V_CommFunction import GetO_BatchWiseLiveStock
+
 from ..Serializer.S_Orders import TC_OrderTermsAndConditionsSerializer
 
 from ..Serializer.S_abc import *
@@ -22,7 +24,7 @@ class AbcView(CreateAPIView):
     def post(self, request, *args, **kwargs):
         try:
             with transaction.atomic():
-                file_serializer =  AbcSerializer(data=request.data)
+                file_serializer =  TC_OrderTermsAndConditionsSerializer(data=request.data)
                 if file_serializer.is_valid():
                     file_serializer.save()
                     return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'File Uploaded Successfully', 'Data': []})
@@ -37,14 +39,15 @@ class AbcView(CreateAPIView):
         try:
             with transaction.atomic():
                 print('aaaaa')
-                Modulesdata = TC_OrderTermsAndConditions.objects.filter(Order=62)
+                Modulesdata = TC_OrderTermsAndConditions.objects.filter(Order=201)
                 print('bbbbb')
                 if Modulesdata.exists():
                     print('ccccc')
                     Modules_Serializer = TC_OrderTermsAndConditionsSerializer(
                     Modulesdata, many=True)
-                    print(Modules_Serializer.data)
-                    return JsonResponse({'StatusCode': 200, 'Status': True,'Message': '','Data': Modules_Serializer.data })
+                    Stock=GetO_BatchWiseLiveStock(44,4)
+                    print(Stock)
+                    return JsonResponse({'StatusCode': 200, 'Status': True,'Message': float(Stock),'Data': Modules_Serializer.data })
                 return JsonResponse({'StatusCode': 204, 'Status': True,'Message':  'Modules Not available', 'Data': []})    
         except Exception as e :
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':   'Execution Error', 'Data':[e]})
