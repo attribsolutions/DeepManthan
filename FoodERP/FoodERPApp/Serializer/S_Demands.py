fcaca8ac07c4b50f9db8a791645604dc2696c9a5
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-01-23 15:42:15 +0530

    Update S_Demands.py

--- FoodERP/FoodERPApp/Serializer/S_Demands.py
+++ FoodERP/FoodERPApp/Serializer/S_Demands.py
@@ -75,17 +75,13 @@ class T_DemandSerializer(serializers.ModelSerializer):
         return instance
 
 
-class InterBranchInwardReferanceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model =TC_InterBranchInwardReferences
-        fields = ['Demand','IsInward']   
+
     
 class T_DemandSerializerSecond(serializers.ModelSerializer):
     Customer = PartiesSerializerSecond(read_only=True)
     Supplier = PartiesSerializerSecond(read_only=True)
     BillingAddress = PartyAddressSerializerSecond(read_only=True) 
     ShippingAddress = PartyAddressSerializerSecond(read_only=True) 
-    DemandReferences=InterBranchInwardReferanceSerializer(many=True)
     class Meta:
         model = T_Demands
         fields = '__all__'
@@ -130,7 +126,6 @@ class TC_DemandSerializerThird(serializers.ModelSerializer):
     Customer = PartiesSerializerThird(read_only=True)
     Supplier = PartiesSerializerThird(read_only=True)
     DemandItem = TC_DemandItemsSerializerSecond(read_only=True,many=True)
-    DemandReferences=InterBranchInwardReferanceSerializer(many=True)
     BillingAddress=PartyAddressSerializerSecond(read_only=True) 
     ShippingAddress=PartyAddressSerializerSecond(read_only=True) 
 
