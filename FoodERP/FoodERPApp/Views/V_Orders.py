f552bea9a67faec36aa5461ee2c7198d12b562db
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-02-08 11:56:15 +0530

    Update V_Orders.py

--- FoodERP/FoodERPApp/Views/V_Orders.py
+++ FoodERP/FoodERPApp/Views/V_Orders.py
@@ -337,7 +337,7 @@ left join m_marginmaster on m_marginmaster.id=a.Margin_id group by Item_id Order
                      
                         UnitDetails.append({
                             "UnitID": d['id'],
-                            "UnitName": d['UnitID']['BaseUnitConversion'] ,
+                            "UnitName": d['BaseUnitConversion'] ,
                             "BaseUnitQuantity": d['BaseUnitQuantity'],
                             "PODefaultUnit": d['PODefaultUnit'],
                             "SODefaultUnit": d['SODefaultUnit'],
