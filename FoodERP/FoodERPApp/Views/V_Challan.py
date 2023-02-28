c1e8c42776cf25b84dbe59a6f5671fff3c93ebc6
pradnyaubale <pradnyaubale12@gmail.com>
2023-02-28 15:24:46 +0530

    Update V_Challan.py

--- FoodERP/FoodERPApp/Views/V_Challan.py
+++ FoodERP/FoodERPApp/Views/V_Challan.py
@@ -63,7 +63,7 @@ class ChallanItemStockView(CreateAPIView):
                             "LiveBatcheGSTID" : d['LiveBatche']['GST']['id'],
                             "Rate":d['LiveBatche']['Rate'],
                             "MRP" : d['LiveBatche']['MRP']['MRP'],
-                            "GSTPercentage" : d['LiveBatche']['GST']['GSTPercentage'],
+                            "GST" : d['LiveBatche']['GST']['GSTPercentage'],
                             "UnitName":d['Unit']['BaseUnitConversion'], 
                             "BaseUnitQuantity":d['BaseUnitQuantity'], 
                             }) 
