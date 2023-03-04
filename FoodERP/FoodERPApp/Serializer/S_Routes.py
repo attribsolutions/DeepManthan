f03e515b9f7f36e413d61116583c5a8537c1951e
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-03-04 18:41:34 +0530

    Update S_Routes.py

--- FoodERP/FoodERPApp/Serializer/S_Routes.py
+++ FoodERP/FoodERPApp/Serializer/S_Routes.py
@@ -1,8 +1,8 @@
 from dataclasses import fields
 from django.forms import SlugField
 from rest_framework import serializers
-
 from ..models import *
+
 
 
 
@@ -12,6 +12,23 @@ class RoutesSerializer(serializers.ModelSerializer):
     class Meta:
         model = M_Routes
         fields = ['id', 'Name', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'IsActive', 'CreatedBy', 'UpdatedBy', 'Company', 'Party']
-
+        
+class RouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  M_Routes
+        fields = '__all__'        
+        
+class PartySerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  M_Parties
+        fields = '__all__'
+                 
+class RoutesUpdateListSerializer(serializers.ModelSerializer):
+    Party = PartySerializer(read_only=True)
+    SubParty = PartySerializer(read_only=True)
+    Route = RoutesSerializer()
+    class Meta:
+        model = MC_PartySubParty
+        fields = '__all__'
 
 
