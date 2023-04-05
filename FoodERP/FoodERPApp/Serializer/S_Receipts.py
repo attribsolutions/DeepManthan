a80047882f2492681c9cac53ea1ab7c9e6b71048
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-04-05 16:26:33 +0530

    Changes

--- FoodERP/FoodERPApp/Serializer/S_Receipts.py
+++ FoodERP/FoodERPApp/Serializer/S_Receipts.py
@@ -1,3 +1,17 @@
 from ..models import *
+from rest_framework import serializers
 
 
+
+
+
+class ReceiptInvoiceserializer(serializers.Serializer):
+   
+    Receipt_id=serializers.IntegerField()
+    Invoice_ID=serializers.IntegerField()
+    InvoiceDate = serializers.DateField()
+    FullInvoiceNumber=serializers.CharField(max_length=100)
+    GrandTotal=serializers.DecimalField(max_digits=10, decimal_places=2)  
+    PaidAmount=serializers.DecimalField(max_digits=10, decimal_places=2)  
+    BalAmt=serializers.DecimalField(max_digits=10, decimal_places=2)  
+    
--- FoodERP/FoodERPApp/Views/V_Receipts.py
+++ FoodERP/FoodERPApp/Views/V_Receipts.py
@@ -21,9 +21,20 @@ class ReceiptInvociesViewList(CreateAPIView):
                 Receiptdata = JSONParser().parse(request)
                 Party = Receiptdata['Party']
                 Customer = Receiptdata['Customer']
-                Receiptinvoicequery = TC_OrderItems.objects.raw('''SELECT '2' aa,'0' flag,tc_receiptinvoices.Receipt_id,t_invoices.id as Invoice_ID ,t_invoices.InvoiceDate, t_invoices.GrandTotal,SUM(IFNULL(tc_receiptinvoices.PaidAmount,0)) PaidAmount,(t_invoices.GrandTotal - SUM(IFNULL(tc_receiptinvoices.PaidAmount,0)))  BalAmt FROM t_invoices LEFT JOIN tc_receiptinvoices ON t_invoices.id=tc_receiptinvoices.Invoice_id and tc_receiptinvoices.flag=0 WHERE t_invoices.id NOT IN (SELECT Invoice_ID FROM (SELECT Invoice_id,tc_receiptinvoices.GrandTotal,SUM(PaidAmount) PaidAmount FROM tc_receiptinvoices JOIN t_invoices  ON t_invoices.id= tc_receiptinvoices.Invoice_id and tc_receiptinvoices.flag=0 WHERE t_invoices.Party_id=%s AND t_invoices.Customer_id=%s GROUP BY t_invoices.id ) Invoicess WHERE (GrandTotal-PaidAmount)=0) AND t_invoices.Party_id=%s AND t_invoices.Customer_id=%s GROUP BY t_invoices.id	''', ([Party], [Customer]))
+                Receiptinvoicequery = TC_ReceiptInvoices.objects.raw('''SELECT '2' aa,'0' flag,tc_receiptinvoices.Receipt_id,t_invoices.id as Invoice_ID ,t_invoices.InvoiceDate,t_invoices.FullInvoiceNumber, t_invoices.GrandTotal,SUM(IFNULL(tc_receiptinvoices.PaidAmount,0)) PaidAmount,(t_invoices.GrandTotal - SUM(IFNULL(tc_receiptinvoices.PaidAmount,0)))  BalAmt FROM t_invoices LEFT JOIN tc_receiptinvoices ON t_invoices.id=tc_receiptinvoices.Invoice_id and tc_receiptinvoices.flag=0 WHERE t_invoices.id NOT IN (SELECT Invoice_ID FROM (SELECT Invoice_id,tc_receiptinvoices.GrandTotal,SUM(PaidAmount) PaidAmount FROM tc_receiptinvoices JOIN t_invoices  ON t_invoices.id= tc_receiptinvoices.Invoice_id and tc_receiptinvoices.flag=0 WHERE t_invoices.Party_id=%s AND t_invoices.Customer_id=%s GROUP BY t_invoices.id ) Invoicess WHERE (GrandTotal-PaidAmount)=0) AND t_invoices.Party_id=%s AND t_invoices.Customer_id=%s GROUP BY t_invoices.id	''', ([Party], [Customer], [Party], [Customer]))
                 print(str(Receiptinvoicequery.query))
-                return JsonResponse({'StatusCode': 200, 'Status': True,  'Message': 'Order Save Successfully', 'Data': []})
-              
+                OrderItemSerializer = ReceiptInvoiceserializer(Receiptinvoicequery, many=True).data
+                ReceiptInvoiceList = list()
+                for a in OrderItemSerializer:
+                    ReceiptInvoiceList.append({
+                        "Receipt":a['Receipt_id'],
+                        "Invoice":a['Invoice_ID'],
+                        "InvoiceDate":a['InvoiceDate'],
+                        "FullInvoiceNumber":a['FullInvoiceNumber'],
+                        "GrandTotal":a['GrandTotal'],
+                        "PaidAmount":a['PaidAmount'],
+                        "BalanceAmount":a['BalAmt']
+                    })
+                return JsonResponse({'StatusCode': 200, 'Status': True,  'Message': '', 'Data': ReceiptInvoiceList})
         except Exception as e:
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  Exception(e), 'Data': []})
