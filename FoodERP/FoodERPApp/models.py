7c34c7a303afc441a9b5f7d0f28343a39f698818
pradnyaubale <87567521+pradnyaubale@users.noreply.github.com>
2023-04-05 15:55:15 +0530

    Update models.py

--- FoodERP/FoodERPApp/models.py
+++ FoodERP/FoodERPApp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
-|from activity_log.models import UserMixin
+from activity_log.models import UserMixin
 
 Create your models here.
 
