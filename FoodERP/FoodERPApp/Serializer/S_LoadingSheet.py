0c4edd73a4fac885ab1639daaecc88605bad755c
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-04-06 17:15:54 +0530

    Update S_LoadingSheet.py

--- FoodERP/FoodERPApp/Serializer/S_LoadingSheet.py
+++ FoodERP/FoodERPApp/Serializer/S_LoadingSheet.py
@@ -32,6 +32,11 @@ class LoadingSheetSerializer(serializers.ModelSerializer):
         LoadingSheetID = T_LoadingSheet.objects.create(**validated_data)
         for LoadingSheet_data in LoadingSheetDetails_data:
             TC_LoadingSheetDetails.objects.create(LoadingSheet=LoadingSheetID, **LoadingSheet_data)
+            print(LoadingSheet_data['Invoice'])
+            print(validated_data[0]['Vehicle'])
+            print(validated_data[0]['Driver'])
+            # InvoiceVehicleandDriverupdate=T_Invoices.objects.filter(id=LoadingSheet_data['Invoice']).update(Vehicle = validated_data[0]['Vehicle'],Driver = validated_data[0]['Driver'])
+            
             
         return LoadingSheetID 
        
