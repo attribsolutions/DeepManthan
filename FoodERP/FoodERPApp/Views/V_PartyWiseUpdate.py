7c1c662e3ec637aeb9e00d7fb9d6c3427db64e8d
pradnyaubale <pradnyaubale12@gmail.com>
2023-04-07 15:57:12 +0530

    Views Update
    views.py
    serializer.py

--- FoodERP/FoodERPApp/Views/V_PartyWiseUpdate.py
+++ FoodERP/FoodERPApp/Views/V_PartyWiseUpdate.py
@@ -55,7 +55,7 @@ class PartyWiseUpdateView(CreateAPIView):
                            
                         elif(Type == 'State'):
                             query1 = M_Parties.objects.filter(id=a['SubParty']['id'])
-                            State_Serializer = SubPartySerializer(query1,many=True).data
+                            State_Serializer = StateSerializer(query1,many=True).data
                             SubPartyListData.append({
                                 "id": a['id'],
                                 "PartyID":a['Party']['id'],
@@ -66,7 +66,7 @@ class PartyWiseUpdateView(CreateAPIView):
                                 })
                         elif(Type == 'District'):
                             query1 = M_Parties.objects.filter(id=a['SubParty']['id'])
-                            State_Serializer = SubPartySerializer(query1,many=True).data
+                            State_Serializer = DistrictSerializer(query1,many=True).data
                             SubPartyListData.append({
                                 "id": a['id'],
                                 "PartyID":a['Party']['id'],
@@ -134,7 +134,8 @@ class PartyWiseUpdateViewSecond(CreateAPIView):
                     elif (Type == 'FSSAINo'):
                         query = MC_PartyAddress.objects.filter(Party=a['SubPartyID'], IsDefault=1).update(FSSAINo=a['Value1'], FSSAIExipry=a['Value2'])
                     elif (Type == 'State'):
-                        query = M_Parties.objects.filter(id=a['SubPartyID']).update(State=a['Value1'])
+                        query = M_Parties.objects.filter(id=a['SubPartyID'])#.update(State=a['Value1'])
+                        print(str(query.query))
                     elif (Type == 'District'):
                         query = M_Parties.objects.filter(id=a['SubPartyID']).update(District=a['Value1'])
                         # print(str(query.query))
