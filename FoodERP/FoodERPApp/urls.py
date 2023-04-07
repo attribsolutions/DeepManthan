a674510769572907a78cad701e82cd3be3ff2aa4
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-04-07 09:55:07 +0530

    Update urls.py

--- FoodERP/FoodERPApp/urls.py
+++ FoodERP/FoodERPApp/urls.py
@@ -406,6 +406,7 @@ urlpatterns = [
 # Receipt
             url(r'ReceiptInvoices$', ReceiptInvoicesView.as_view()),
             url(r'Receipt/([0-9]+)$', ReceiptView.as_view()),
+            url(r'Receipt$', ReceiptView.as_view()),
             url(r'ReceiptFilter$', ReceiptListView.as_view()),
     
 # RoleAccess========================================= 
