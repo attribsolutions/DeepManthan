072b81500921a506ef962be259502b3450f1ce4e
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-04-25 10:45:31 +0530

    creditdebit

--- FoodERP/FoodERPApp/Serializer/S_CreditDebit.py
+++ FoodERP/FoodERPApp/Serializer/S_CreditDebit.py
@@ -113,8 +113,8 @@ class CreditDebitNoteItemSerializerSecond(serializers.ModelSerializer):
     
         
 class SingleCreditDebitNoteThirdSerializer(serializers.ModelSerializer):
-    Customer = PartiesSerializer(read_only=True)
-    Party = PartiesSerializer(read_only=True)
+    Customer = PartiesSerializerSecond(read_only=True)
+    Party = PartiesSerializerSecond(read_only=True)
     NoteReason = GeneralMasterserializer(read_only=True)
     NoteType = GeneralMasterserializer(read_only=True)
     Receipt = ReceiptSerializer(read_only=True)
