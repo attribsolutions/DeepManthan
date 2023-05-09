c6883222e25f7e86b61f3189ffeb3228b61f8005
pradnyaubale <pradnyaubale12@gmail.com>
2023-05-09 12:41:24 +0530

    Update S_SAPApi.py

---
+++ FoodERP/FoodERPApp/Serializer/S_SAPApi.py
@@ -0,0 +1,6 @@
+from ..models import *
+from rest_framework import serializers
+from ..Serializer.S_Items import *
+from ..Serializer.S_Orders import  *
+from ..Serializer.S_Drivers import  *
+from ..Serializer.S_Vehicles import  *
