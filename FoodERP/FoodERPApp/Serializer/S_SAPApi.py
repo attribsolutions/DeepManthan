b8d352ac5db73834e82f5010f18231b82308dc6b
pradnyaubale <pradnyaubale12@gmail.com>
2023-05-10 17:48:22 +0530

    SAPInvoice API

--- FoodERP/FoodERPApp/Serializer/S_SAPApi.py
+++ FoodERP/FoodERPApp/Serializer/S_SAPApi.py
@@ -4,3 +4,47 @@ from ..Serializer.S_Items import *
 from ..Serializer.S_Orders import  *
 from ..Serializer.S_Drivers import  *
 from ..Serializer.S_Vehicles import  *
+
+
+
+class InvoiceItemsSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = TC_InvoiceItems
+        fields = ['BatchCode', 'Quantity', 'BaseUnitQuantity', 'MRP', 'Rate', 'BasicAmount', 'TaxType', 'GST', 'GSTAmount', 'Amount', 'DiscountType', 'Discount', 'DiscountAmount', 'CGST', 'SGST', 'IGST', 'CGSTPercentage', 'SGSTPercentage', 'IGSTPercentage', 'CreatedOn', 'Item', 'Unit', 'BatchDate','LiveBatch','MRPValue','GSTPercentage']   
+
+
+class InvoiceSerializer(serializers.ModelSerializer):
+    InvoiceItems = InvoiceItemsSerializer(many=True)
+    # InvoicesReferences = InvoicesReferencesSerializer(many=True) 
+    # obatchwiseStock=obatchwiseStockSerializer(many=True)
+    class Meta:
+        model = T_Invoices
+        fields = ['InvoiceDate', 'InvoiceNumber', 'FullInvoiceNumber', 'GrandTotal', 'RoundOffAmount', 'CreatedBy', 'UpdatedBy', 'Customer', 'Party', 'InvoiceItems']
+
+    def create(self, validated_data):
+        print('dddddddddddddddddddddddd',validated_data)
+        InvoiceItems_data = validated_data.pop('InvoiceItems')
+        # InvoicesReferences_data = validated_data.pop('InvoicesReferences')
+        # O_BatchWiseLiveStockItems_data = validated_data.pop('obatchwiseStock')
+        InvoiceID = T_Invoices.objects.create(**validated_data)
+        
+        for InvoiceItem_data in InvoiceItems_data:
+            InvoiceItemID =TC_InvoiceItems.objects.create(Invoice=InvoiceID, **InvoiceItem_data)
+            
+        # for O_BatchWiseLiveStockItem_data in O_BatchWiseLiveStockItems_data:
+            
+        #         OBatchQuantity=O_BatchWiseLiveStock.objects.filter(id=O_BatchWiseLiveStockItem_data['Quantity']).values('BaseUnitQuantity')
+        #         print(OBatchQuantity[0]['BaseUnitQuantity'],O_BatchWiseLiveStockItem_data['BaseUnitQuantity'])
+        #         if(OBatchQuantity[0]['BaseUnitQuantity'] >= O_BatchWiseLiveStockItem_data['BaseUnitQuantity']):
+        #             OBatchWiseLiveStock=O_BatchWiseLiveStock.objects.filter(id=O_BatchWiseLiveStockItem_data['Quantity']).update(BaseUnitQuantity =  OBatchQuantity[0]['BaseUnitQuantity'] - O_BatchWiseLiveStockItem_data['BaseUnitQuantity'])
+        #         else:
+                    
+        #             raise serializers.ValidationError("Not In Stock ")    
+          
+        # for InvoicesReference_data in InvoicesReferences_data:
+        #     print(InvoiceID) 
+        #     InvoicesReferences = TC_InvoicesReferences.objects.create(Invoice=InvoiceID, **InvoicesReference_data)   
+              
+        
+        return InvoiceID 
--- FoodERP/FoodERPApp/Views/V_SAPApi.py
+++ FoodERP/FoodERPApp/Views/V_SAPApi.py
@@ -1,42 +1,117 @@
 from django.contrib.auth import authenticate
 from base64 import b64decode
 from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import BasicAuthentication
 from django.db import transaction
+
+from ..Serializer.S_SAPApi import InvoiceSerializer
 from ..models import *
 
 class SAPInvoiceView(CreateAPIView):
     
     permission_classes = (IsAuthenticated,)
-    # authentication_classes = [BasicAuthentication]
-
+    
+    
     @transaction.atomic()
-    def get(self, request, *args, **kwargs):
+    def post(self, request):
         try:
             with transaction.atomic():
-                user='admin'
-                passw='1234'
-                # Get the Authorization header from the request
-                auth_header = request.META.get('HTTP_AUTHORIZATION', '')
-                # Check if the header is a basic authentication header
-                if auth_header.startswith('Basic '):
-                    # Decode the base64-encoded string in the header and extract the username and password
-                    credentials = b64decode(auth_header[6:]).decode('utf-8').split(':')
+                aa = JSONParser().parse(request)
+                
+                CustomerMapping=M_Parties.objects.filter(SAPPartyCode=aa['CustomerID']).values("id")
+                PartyMapping=M_Parties.objects.filter(SAPPartyCode=aa['Plant']).values("id")
+                print(CustomerMapping,PartyMapping)
+                if CustomerMapping.exists:
+                        aa['Customer']=CustomerMapping
+                else:
+                        return JsonResponse({'StatusCode': 406, 'Status': True,  'Message': " Invalid Customer Data ", 'Data':[]})    
+                if PartyMapping.exists:
+                        aa['Party']=PartyMapping
+                else:
+                        return JsonResponse({'StatusCode': 406, 'Status': True,  'Message': " Invalid Customer Data ", 'Data':[]})    
+                        
+                for bb in aa['InvoiceItems']:
                     
-                    username = credentials[0]
-                    password = credentials[1]
-                    print(username,password)
-                    return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'File Uploaded Successfully', 'Data': []})
-                    # if(user = credentials[0]):
-                    #     return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'Authorized user', 'Data': []})
-                    # else:
-                    #     return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'UnAuthorized user', 'Data': []})
+                    ItemMapping=M_Items.objects.filter(SAPItemCode=bb['MaterialCode']).values("id")
+                    UnitMapping=M_Units.objects.filter(SAPUnit=bb['BaseUOM']).values("id")
+                    print(ItemMapping,UnitMapping)
+                    InvoiceItems=list()
+                    if ItemMapping.exists:
+                        bb['Item']=ItemMapping
+                    else:
+                        return JsonResponse({'StatusCode': 406, 'Status': True,  'Message': " Invalid Material Code", 'Data':[]})     
+                    
+                    if UnitMapping.exists:
+                        print('sssssssssssssssssssssss')
+                        MC_UnitID=MC_ItemUnits.objects.filter(UnitID=UnitMapping[0]["id"],Item=ItemMapping[0]["id"],IsDeleted=0).values("id")
+                        print(MC_UnitID)
+                        
+                        if MC_UnitID.exists():
+                            bb['Unit']=MC_UnitID
+                        else:
+                            return JsonResponse({'StatusCode': 406, 'Status': True,  'Message': " Invalid MC_ItemUnits ", 'Data':[]})            
+                    else:
+                        return JsonResponse({'StatusCode': 406, 'Status': True,  'Message': " Invalid Unit", 'Data':[]})
+                    
+                    InvoiceItems.append({
+                                    
+                                "Item": ItemMapping[0]["id"],
+                                "Unit": MC_UnitID[0]["id"],
+                                "BatchCode": bb['BatchCode'],
+                                "Quantity":  bb['Quantity'],
+                                "BatchDate": aa['InvoiceDate'],
+                                "BaseUnitQuantity":1,
+                                "LiveBatch": "",
+                                "MRP": "",
+                                "MRPValue":bb['MRP'],
+                                "Rate": bb['LandedPerUnitRate'],
+                                "BasicAmount": bb['TaxableAmount'],
+                                "GSTAmount": float(bb['CGST'])+float(bb['SGST'])+float(bb['IGST'])+float(bb['UGST']),
+                                "GST": "",
+                                "GSTPercentage": float(bb['CGSTPercentage'])+float(bb['SGSTPercentage'])+float(bb['IGSTPercentage'])+float(bb['UGSTPercentage']),
+                                "CGST": bb['CGST'],
+                                "SGST":bb['SGST'],
+                                "IGST": bb['IGST'],
+                                "CGSTPercentage": bb['CGSTPercentage'],
+                                "SGSTPercentage": bb['SGSTPercentage'],
+                                "IGSTPercentage": bb['IGSTPercentage'],
+                                "Amount": bb['TotalValue'],
+                                "TaxType":'GST',
+                                "DiscountType":1,
+                                "LiveBatch":"",
+                                "Discount":bb['DiscountPercentage'],
+                                "DiscountAmount":bb['DiscountAmount'],
+                        })
+                    InvoiceData=list()
+                    InvoiceData.append({
+                            
+                            "GrandTotal":aa['GrossAmount'],
+                            "RoundOffAmount": "0.00",
+                            "InvoiceNumber": 1,
+                            "FullInvoiceNumber": aa["InvoiceNumber"],
+                            "Customer": CustomerMapping[0]["id"],
+                            "Party": PartyMapping[0]["id"],
+                            "CreatedBy": 30,
+                            "UpdatedBy": 30,
+                           
+                            "InvoiceDate": aa['InvoiceDate'],
+                            "InvoiceItems": InvoiceItems
 
+                    })   
+                    print(InvoiceData[0])
+                   
+                    Invoice_serializer = InvoiceSerializer(data=InvoiceData[0])
+                    if Invoice_serializer.is_valid():
+                        Invoice_serializer.save()
+                    else:
+                        transaction.set_rollback(True)
+                        return JsonResponse({'StatusCode': 406, 'Status': True,  'Message': Invoice_serializer.errors, 'Data': []})
+                return JsonResponse({'StatusCode': 200, 'Status': True,  'Message': 'Invoice Save Successfully', 'Data':[]})
         except Exception as e:
-            raise JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  Exception(e), 'Data':[]})
-
+            return JsonResponse({'StatusCode': 400, 'Status': True, 'Message': e, 'Data': []})
 
 
 
