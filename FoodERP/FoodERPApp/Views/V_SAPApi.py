886a8347a84dc3a1a392c81d6fa218eb44ed65fa
pradnyaubale <pradnyaubale12@gmail.com>
2023-05-25 14:51:09 +0530

    Update V_SAPApi.py

--- FoodERP/FoodERPApp/Views/V_SAPApi.py
+++ FoodERP/FoodERPApp/Views/V_SAPApi.py
@@ -139,7 +139,7 @@ class SAPOrderView(CreateAPIView):
 
                 response = requests.request("POST", url, headers=headers, data=payload)
                 print(response)
-                return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'Order Send to SAP Successfully ', 'Data': []})
+                return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'Order Send to SAP Successfully ', 'Data': response})
         except Exception as e:
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  Exception(e), 'Data': []})
               
