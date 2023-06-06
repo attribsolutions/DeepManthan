82123eee37b0e36decd0d5249751da2af3e9ad06
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-06-06 15:51:57 +0530

    Update settings.py

--- FoodERP/FoodERP/settings.py
+++ FoodERP/FoodERP/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t#s!16-8)sy91!+@q2hmdt_yclkuldlx=*g5aw_cb&^+rzr@ty'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['10.1.201.19','103.135.203.145','192.168.1.114','117.248.109.234'] 
+ALLOWED_HOSTS = ['10.1.201.19','103.135.203.145','192.168.1.114','117.248.109.234','127.0.0.1'] 
 
 # Application definition
 CORS_ORIGIN_ALLOW_ALL = True #we allow the all domain to access through API
