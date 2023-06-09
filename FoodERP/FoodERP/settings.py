cdcc1749f85741e7ca9419826cf59f5e69f67804
pradnyaubale <pradnyaubale12@gmail.com>
2023-06-09 12:17:41 +0530

    Update settings.py

--- FoodERP/FoodERP/settings.py
+++ FoodERP/FoodERP/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'FoodERP.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'devfooderp20230607',     
+        'NAME': 'fooderp20230607',     
         'USER': 'pk',
         'PASSWORD': 'P@ssw0rd', 
-        'HOST': '192.168.1.114', 
+        'HOST': '10.1.201.19', 
         'PORT': '3306' 
     }
  
