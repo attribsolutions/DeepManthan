3ab96fe89ce96ef379260fd50ba87186f291886d
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-06-10 12:10:58 +0530

    CityList
    urls.py
    V_States.py

--- FoodERP/FoodERPApp/urls.py
+++ FoodERP/FoodERPApp/urls.py
@@ -330,6 +330,8 @@ urlpatterns = [
             url(r'GetDistrictOnState/([0-9]+)$',M_DistrictView.as_view()),
             url(r'GetCityOnDistrict/([0-9]+)$',M_CitiesView.as_view()),
             url(r'Cities$',M_CitiesView.as_view()),
+            url(r'CityList$',CitiesListView.as_view()),
+            
 
 # PartySubParty
             url(r'PartySubParty/([0-9]+)$',PartySubPartyViewSecond.as_view()),
