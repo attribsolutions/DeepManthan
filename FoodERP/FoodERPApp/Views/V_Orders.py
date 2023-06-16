36bf0137049a888a2bc5c2c0833541294a80da22
pradnyaubale <pradnyaubale12@gmail.com>
2023-06-16 18:18:34 +0530

    Update V_Orders.py

--- FoodERP/FoodERPApp/Views/V_Orders.py
+++ FoodERP/FoodERPApp/Views/V_Orders.py
@@ -743,9 +743,9 @@ class SummaryReportView(CreateAPIView):
                                     "OrderDate": a['OrderDate'],
                                     "SupplierName": a['Supplier']['Name'],
                                     "CustomerName": a['Customer']['Name'],
-                                    "QtyInNo": round(float(b['QtyInNo']),3),
-                                    "QtyInKg": round(float(b['QtyInKg']),3),
-                                    "QtyInBox": round(float(b['QtyInBox']),3),
+                                    "QtyInNo": float(b['QtyInNo']),
+                                    "QtyInKg": float(b['QtyInKg']),
+                                    "QtyInBox": float(b['QtyInBox']),
                                     "OrderAmount": a['OrderAmount']
                                 }) 
                     return JsonResponse({'StatusCode': 200, 'Status': True,'Data': OrderItemDetails })
