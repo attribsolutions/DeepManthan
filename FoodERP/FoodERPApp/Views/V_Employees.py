a1f2382826d079394525b2126b84f620ce7304b7
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-06-23 15:48:44 +0530

    Update V_Employees.py

--- FoodERP/FoodERPApp/Views/V_Employees.py
+++ FoodERP/FoodERPApp/Views/V_Employees.py
@@ -283,6 +283,8 @@ class ManagementEmployeePartiesFilterView(CreateAPIView):
                         'Party': a['PartyID']
                     })
 
+
+
                 # q1=M_PartyType.objects.filter(Company=CompanyID,IsRetailer=0,IsSCM=1)
                 # q0 = MC_ManagementParties.objects.filter(Employee=EmployeeID).select_related('Party')
                 # print(str(q0.query))
