cb067194b7a06f6c533d1e37d40db17f6af3d8dd
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-07-04 16:18:49 +0530

    Update V_PurchaseReturn.py

--- FoodERP/FoodERPApp/Views/V_PurchaseReturn.py
+++ FoodERP/FoodERPApp/Views/V_PurchaseReturn.py
@@ -257,12 +257,51 @@ class ReturnItemBatchCodeAddView(CreateAPIView):
                 PurchaseReturndata = JSONParser().parse(request)
                 ItemID = PurchaseReturndata['ItemID']
                 BatchCode = PurchaseReturndata['BatchCode']
-                query = TC_GRNItems.objects.filter(Item=ItemID,BatchCode=BatchCode).order_by('id')[:1]
+                if BatchCode != "":
+               
+                    query = TC_GRNItems.objects.filter(Item=ItemID,BatchCode=BatchCode).order_by('id')[:1]
+                else:
+                    query = TC_GRNItems.objects.filter(Item=ItemID).order_by('id')[:1]  
                 if query.exists():
                     GRNItemsdata = TC_GRNItemsSerializerSecond(query, many=True).data
                     # return JsonResponse({'query':  Itemsdata})
                     GRMItems = list()
                     for a in GRNItemsdata:
+                        Item =a['Item']['id']
+                        Unitquery = MC_ItemUnits.objects.filter(Item_id=Item,IsDeleted=0)
+                        if Unitquery.exists():
+                            Unitdata = Mc_ItemUnitSerializerThird(Unitquery, many=True).data
+                            ItemUnitDetails = list()
+                            for c in Unitdata:
+                                ItemUnitDetails.append({
+                                "Unit": c['id'],
+                                "BaseUnitQuantity": c['BaseUnitQuantity'],
+                                "IsBase": c['IsBase'],
+                                "UnitName": c['BaseUnitConversion'],
+                            })
+                        
+                        MRPquery = M_MRPMaster.objects.filter(Item_id=Item).order_by('-id')[:3] 
+                        if MRPquery.exists():
+                            MRPdata = ItemMRPSerializerSecond(MRPquery, many=True).data
+                            ItemMRPDetails = list()
+                            
+                            for d in MRPdata:
+                                ItemMRPDetails.append({
+                                "MRP": d['id'],
+                                "MRPValue": d['MRP'],   
+                            })
+                        
+                        GSTquery = M_GSTHSNCode.objects.filter(Item_id=Item).order_by('-id')[:3] 
+                        if GSTquery.exists():
+                            Gstdata = ItemGSTHSNSerializerSecond(GSTquery, many=True).data
+                            ItemGSTDetails = list()
+                            for e in Gstdata:
+                                ItemGSTDetails.append({
+                                "GST": e['id'],
+                                "GSTPercentage": e['GSTPercentage'],   
+                            }) 
+                        
+                        
                         GRMItems.append({
                             "Item": a['Item']['id'],
                             "ItemName": a['Item']['Name'],
@@ -287,9 +326,9 @@ class ReturnItemBatchCodeAddView(CreateAPIView):
                             "Amount": a['Amount'],
                             "BatchCode": a['BatchCode'],
                             "BatchDate": a['BatchDate'],
-                            # "ItemUnitDetails": ItemUnitDetails, 
-                            # "ItemMRPDetails":ItemMRPDetails,
-                            # "ItemGSTDetails":ItemGSTDetails
+                            "ItemUnitDetails": ItemUnitDetails, 
+                            "ItemMRPDetails":ItemMRPDetails,
+                            "ItemGSTDetails":ItemGSTDetails
                         })   
                     return JsonResponse({'StatusCode': 200, 'Status': True, 'Data': GRMItems})
                 return JsonResponse({'StatusCode': 204, 'Status': True, 'Message': 'Items Not available ', 'Data': []})
