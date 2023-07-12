4179ad42bc8b0a1a13eb68def3e79a2fcf627f1c
pradnyaubale <pradnyaubale12@gmail.com>
2023-07-12 16:15:46 +0530

    Update settings.py

--- FoodERP/FoodERP/settings.py
+++ FoodERP/FoodERP/settings.py
@@ -82,13 +82,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
 
-        'NAME': 'FoodERP',     
+        'NAME': 'devfooderp20230706',     
 
         'USER': 'pk',
         'PASSWORD': 'P@ssw0rd',  
 
 
-        'HOST': '10.4.5.64',   
+        'HOST': '192.168.1.114',   
 
 
 
