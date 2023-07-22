2e80b85eaae05621c7c31a7022db8369ccb335c1
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-07-22 18:10:25 +0530

    Update V_PurchaseReturn.py

--- FoodERP/FoodERPApp/Views/V_PurchaseReturn.py
+++ FoodERP/FoodERPApp/Views/V_PurchaseReturn.py
@@ -271,19 +271,11 @@ class PurchaseReturnView(CreateAPIView):
     def delete(self, request, id=0):
         try:
             with transaction.atomic():
-                O_BatchWiseLiveStockData = O_BatchWiseLiveStock.objects.filter(PurchaseReturn_id=id).values('OriginalBaseUnitQuantity','BaseUnitQuantity')
-                if O_BatchWiseLiveStockData:
-                    for a in O_BatchWiseLiveStockData:
-                        if (a['OriginalBaseUnitQuantity'] != a['BaseUnitQuantity']) :
-                            return JsonResponse({'StatusCode': 226, 'Status': True, 'Message': 'Return  Used in another Transaction', 'Data': []})   
-                    
-                    PurchaseReturn_Data = T_PurchaseReturn.objects.get(id=id)
-                    PurchaseReturn_Data.delete()
-                    return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'Return Deleted Successfully', 'Data': []})
-                else:
-                    Query = T_PurchaseReturn.objects.filter(id=id)
-                    if Query.exists():
-                        PurchaseReturnSerializer = PurchaseReturnSerializerThird(Query, many=True).data 
+                Query = TC_PurchaseReturnReferences.objects.filter(PurchaseReturn=id)
+                if Query:
+                    Query2 = T_PurchaseReturn.objects.filter(id=id)
+                    if Query2.exists():
+                        PurchaseReturnSerializer = PurchaseReturnSerializerThird(Query2, many=True).data 
                         for a in PurchaseReturnSerializer:
                             for b in a['ReturnItems']:
                                 Qty =0.00 
@@ -296,7 +288,12 @@ class PurchaseReturnView(CreateAPIView):
                                     return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'Return Qty greater than Consolidated return qty', 'Data': []})     
                         PurchaseReturn_Data = T_PurchaseReturn.objects.get(id=id)
                         PurchaseReturn_Data.delete()        
-                        return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'Return Deleted Successfully', 'Data': []})
+                        return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'Return Deleted Successfully', 'Data': []}) 
+                else:
+                    PurchaseReturn_Data = T_PurchaseReturn.objects.get(id=id)
+                    PurchaseReturn_Data.delete()
+                    return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'Return Deleted Successfully', 'Data': []})
+                    
         except T_PurchaseReturn.DoesNotExist:
             return JsonResponse({'StatusCode': 204, 'Status': True, 'Message': 'Record Not available', 'Data': []})
         except IntegrityError:
