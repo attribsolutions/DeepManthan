07144113fe21ec97fc912d188ad0beff6cff566d
pradnyaubale <pradnyaubale12@gmail.com>
2023-07-29 16:59:13 +0530

    s

--- FoodERP/FoodERPApp/Serializer/S_Claim.py
+++ FoodERP/FoodERPApp/Serializer/S_Claim.py
@@ -53,10 +53,16 @@ class MasterclaimReasonReportSerializer(serializers.Serializer):
     ClaimAgainstNetSale =serializers.DecimalField(max_digits=10, decimal_places=2)    
 
 
-class ReasonwiseMasterClaimSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MC_ReturnReasonwiseMasterClaim
-        fields = '__all__'  
+class ReasonwiseMasterClaimSerializer(serializers.Serializer):
+    id= serializers.IntegerField()
+    ItemReasonName = serializers.CharField(max_length=500)
+    PrimaryAmount= serializers.DecimalField(max_digits=10, decimal_places=2)
+    SecondaryAmount= serializers.DecimalField(max_digits=10, decimal_places=2)
+    ReturnAmount= serializers.DecimalField(max_digits=10, decimal_places=2)
+    NetSaleValue = serializers.DecimalField(max_digits=10, decimal_places=2)
+    Budget=serializers.DecimalField(max_digits=10, decimal_places=2)
+    ClaimAmount=serializers.DecimalField(max_digits=10, decimal_places=2)
+    ClaimAgainstNetSale =serializers.DecimalField(max_digits=10, decimal_places=2)    
 
 
 class ProductwiseMasterClaimSerializer(serializers.ModelSerializer):
