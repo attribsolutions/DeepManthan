2a2221008fd9dee675761f44bf61987f4dcf3422
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-08-14 11:48:48 +0530

    Changes

--- FoodERP/FoodERPApp/Views/V_R3B_Reports.py
+++ FoodERP/FoodERPApp/Views/V_R3B_Reports.py
@@ -44,13 +44,14 @@ UNION
 SELECT 1 as id, '(d) Inward supplies (liable to reverse charge) ' A, 0 Taxablevalue,0 IGST,0 CGST, 0 SGST,0 Cess
 UNION 
 SELECT 1 as id, '(e) Non-GST Outward supplies' A, 0 Taxablevalue,0 IGST,0 CGST, 0 SGST,0 Cess''',([Party],[FromDate],[ToDate],[Party],[FromDate],[ToDate]))
+        print(str(query.query))
         DOSAISLTRCdata = DOSAISLTRCSerializer(query, many=True).data
         df=pd.DataFrame(DOSAISLTRCdata)
         # print (df)
 
         specific_column_names = {
         'A':'Nature of Supplies', 
-        'TaxableValue':'Total Taxable value',
+        'Taxablevalue':'Total Taxable value',
         'IGST':'Integrated Tax',
         'CGST':'Central Tax',
         'SGST':'State/UT Tax',
@@ -70,34 +71,34 @@ SELECT 1 as id, '(e) Non-GST Outward supplies' A, 0 Taxablevalue,0 IGST,0 CGST, 
                 
 ################################## 4.EligibleITC #############################################################################
 
-        EligibleITCquery = T_Invoices.objects.raw('''SELECT '(A) ITC Available (Whether in full or part)' A,0 IGST,0 CGST, 0 SGST,0 Cess
+        EligibleITCquery = T_Invoices.objects.raw('''SELECT 1 as id, '(A) ITC Available (Whether in full or part)' A,0 IGST,0 CGST, 0 SGST,0 Cess
 UNION
-SELECT '(1)   Import of goods ' A, 0 IGST,0 CGST, 0 SGST,0 Cess
+SELECT 1 as id,  '(1)   Import of goods ' A, 0 IGST,0 CGST, 0 SGST,0 Cess
 UNION
-SELECT '(2)   Import of services' A,0 IGST,0 CGST, 0 SGST,0 Cess
+SELECT 1 as id, '(2)   Import of services' A,0 IGST,0 CGST, 0 SGST,0 Cess
 UNION
-SELECT '(3)   Inward supplies liable to reverse charge(other than 1 &2 above)' A, 0 IGST,0 CGST, 0 SGST,0 Cess
+SELECT 1 as id, '(3)   Inward supplies liable to reverse charge(other than 1 &2 above)' A, 0 IGST,0 CGST, 0 SGST,0 Cess
 UNION
-SELECT '(4)   Inward supplies from ISD' A, 0 IGST,0 CGST, 0 SGST,0 Cess
+SELECT 1 as id, '(4)   Inward supplies from ISD' A, 0 IGST,0 CGST, 0 SGST,0 Cess
 UNION
-SELECT '(5)   All other ITC' A,IFNULL(SUM(TC_InvoiceItems.IGST),0) IGST,IFNULL(sum(TC_InvoiceItems.CGST),0) CGST, IFNULL(SUM(TC_InvoiceItems.SGST),0) SGST,'0'Cess
+SELECT 1 as id, '(5)   All other ITC' A,IFNULL(SUM(TC_InvoiceItems.IGST),0) IGST,IFNULL(sum(TC_InvoiceItems.CGST),0) CGST, IFNULL(SUM(TC_InvoiceItems.SGST),0) SGST,'0'Cess
 FROM T_Invoices
 JOIN TC_InvoiceItems ON TC_InvoiceItems.Invoice_id=T_Invoices.id
 WHERE  T_Invoices.Customer_id=%s AND T_Invoices.InvoiceDate BETWEEN %s AND %s
 UNION
-SELECT ' (B) ITC Reversed' A, 0 IGST,0 CGST, 0 SGST,0 Cess
+SELECT 1 as id, ' (B) ITC Reversed' A, 0 IGST,0 CGST, 0 SGST,0 Cess
 UNION
-SELECT '(1)   As per Rule 42 & 43 of SGST/CGST rules ' A, 0 IGST,0 CGST, 0 SGST,0 Cess
+SELECT 1 as id, '(1)   As per Rule 42 & 43 of SGST/CGST rules ' A, 0 IGST,0 CGST, 0 SGST,0 Cess
 UNION
-SELECT '(2)   Others' A, 0 IGST,0 CGST, 0 SGST,0 Cess
+SELECT 1 as id, '(2)   Others' A, 0 IGST,0 CGST, 0 SGST,0 Cess
 UNION
 SELECT '(C) Net ITC Available (A)-(B)' A, 0 IGST,0 CGST, 0 SGST,0 Cess
 UNION
-SELECT ' (D) Ineligible ITC' A, 0 IGST,0 CGST, 0 SGST,0 Cess
+SELECT 1 as id, ' (D) Ineligible ITC' A, 0 IGST,0 CGST, 0 SGST,0 Cess
 UNION
-SELECT '(1)   As per section 17(5) of CGST//SGST Act' A, 0 IGST,0 CGST, 0 SGST,0 Cess
+SELECT 1 as id, '(1)   As per section 17(5) of CGST//SGST Act' A, 0 IGST,0 CGST, 0 SGST,0 Cess
 UNION
-SELECT '(2)   Others' A, 0 IGST,0 CGST, 0 SGST,0 Cess''',([Party],[FromDate],[ToDate]))
+SELECT 1 as id, '(2)   Others' A, 0 IGST,0 CGST, 0 SGST,0 Cess''',([Party],[FromDate],[ToDate]))
         EgibleITCdata = EligibleITCSerializer(EligibleITCquery, many=True).data
         df2=pd.DataFrame(EgibleITCdata)
 
@@ -121,9 +122,6 @@ SELECT '(2)   Others' A, 0 IGST,0 CGST, 0 SGST,0 Cess''',([Party],[FromDate],[To
             for row_idx, value in enumerate(df2[header], start=2):
                 ws2.cell(row=row_idx, column=col_idx, value=value) 
         
-
-
-        
         
 #####################   3.2 Of the supplies shown in 3.1 (a), details of inter-state supplies made to unregistered persons, composition taxable person and UIN holders#################################################
         
@@ -140,7 +138,7 @@ WHERE  Party_id=%s AND T_Invoices.InvoiceDate BETWEEN %s AND %s   group by M_Sta
         
         specific_column_names = {
         'states':'Place of Supply(State/UT)', 
-        'TaxableValue' :'Taxable Value',
+        'Taxablevalue' :'Taxable Value',
         'IGST':'Amount of Integrated Tax',
         }
         
