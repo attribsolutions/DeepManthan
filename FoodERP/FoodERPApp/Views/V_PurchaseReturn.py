e36b7e008b4cd14c430dac6a7e636c161216bbb7
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-09-14 16:33:29 +0530

    Update V_PurchaseReturn.py

--- FoodERP/FoodERPApp/Views/V_PurchaseReturn.py
+++ FoodERP/FoodERPApp/Views/V_PurchaseReturn.py
@@ -390,7 +390,7 @@ class PurchaseReturnView(CreateAPIView):
             print('DDDDD')
             
             # log_entry = create_transaction_logNew(request, PurchaseReturndata, 0,  Exception(e),33,0)
-            return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  Exception(e), 'Data':[]})
+            return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  e, 'Data':[]})
     
     # Purchase Return DELETE API New code Date 25/07/2023
     @transaction.atomic()
