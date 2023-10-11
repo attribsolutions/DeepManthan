fefddbb6ee3eae56f94a348faa5b085361869dc5
pradnyaubale <pradnyaubale12@gmail.com>
2023-10-11 11:03:21 +0530

    Update settings.py

--- FoodERP/FoodERP/settings.py
+++ FoodERP/FoodERP/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'FoodERP.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'FoodERP',     
+        'NAME': 'devfooderp20231005',     
         'USER': 'pk',
         'PASSWORD': 'P@ssw0rd',  
-        'HOST': '10.4.5.64',
+        'HOST': '192.168.1.114',
         'PORT': '3306' , 
         'OPTIONS': { 
             'sql_mode': 'STRICT_TRANS_TABLES', 
@@ -158,9 +158,9 @@ USE_TZ = False
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-MEDIA_URL = '/home/admin1/DeepManthan/FoodERP/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/Images')
-# MEDIA_ROOT = '/media/'
+# MEDIA_URL = '/home/admin1/DeepManthan/FoodERP/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
