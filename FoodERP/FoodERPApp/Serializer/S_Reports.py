e819eb9f770236eecb3984dc4026b18e40597b48
pradnyaubale <pradnyaubale12@gmail.com>
2023-10-12 18:10:31 +0530

    RouteID in RetailerData Report

--- FoodERP/FoodERPApp/Serializer/S_Reports.py
+++ FoodERP/FoodERPApp/Serializer/S_Reports.py
@@ -224,6 +224,7 @@ class RetailerDataExportSerializer(serializers.Serializer):
     Latitude=serializers.CharField(max_length=100)
     Longitude=serializers.CharField(max_length=100)
     SAPPartyCode = serializers.CharField(max_length=500)
+    Routeid =serializers.IntegerField()
     
     
     
