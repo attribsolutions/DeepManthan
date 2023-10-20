6c339c667697b1dfeadc728a7c7232a2cd6bde7d
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2023-10-20 16:24:06 +0530

    Invoices

--- FoodERP/FoodERPApp/Views/V_Invoices.py
+++ FoodERP/FoodERPApp/Views/V_Invoices.py
@@ -640,14 +640,18 @@ class BulkInvoiceView(CreateAPIView):
                         UnitMapping=M_UnitMappingMaster.objects.filter(MapUnit=bb['Unit'],Party=aa['Party']).values("Unit")
                         if UnitMapping.count() > 0:
                             MC_UnitID=MC_ItemUnits.objects.filter(UnitID=UnitMapping[0]["Unit"],Item=ItemMapping[0]["Item"],IsDeleted=0).values("id")
+                            
                             if MC_UnitID.count() > 0:
                                 bb['Unit']=MC_UnitID[0]['id']
+                                bb['BaseUnitQuantity']=UnitwiseQuantityConversion(ItemMapping[0]["Item"],bb['Quantity'],bb['Unit'],0,0,0,0).GetBaseUnitQuantity()
                             else:
                                 # log_entry = create_transaction_logNew(request, Invoicedata, 0, " MC_ItemUnits Data Mapping Missing",39,0)
                                 return JsonResponse({'StatusCode': 406, 'Status': True,  'Message': " MC_ItemUnits Data Mapping Missing", 'Data':[]})            
                         else:
                             # log_entry = create_transaction_logNew(request, Invoicedata, 0, "Unit Data Mapping Missing",40,0)
                             return JsonResponse({'StatusCode': 406, 'Status': True,  'Message': " Unit Data Mapping Missing", 'Data':[]})
+                         
+                    # return JsonResponse({'StatusCode': 200, 'Status': True,  'Message': 'Invoice Save Successfully', 'Data':aa })    
                     Invoice_serializer = BulkInvoiceSerializer(data=aa)
                     if Invoice_serializer.is_valid():
                         Invoice_serializer.save()
