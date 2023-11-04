9207df9e9736b40c502b67848455f623e687d311
pradnyaubale <pradnyaubale12@gmail.com>
2023-11-04 18:39:19 +0530

    gg

--- FoodERP/FoodERPApp/Views/V_PartyDetails.py
+++ FoodERP/FoodERPApp/Views/V_PartyDetails.py
@@ -100,5 +100,58 @@ class PartyDetailsView(CreateAPIView):
         except Exception as e:
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  Exception(e), 'Data':[]})
         
+class GetPartydetailsView(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    def get(self, request, Employee=0,Group=0):
+        try:
+            with transaction.atomic():
+                
+                EmpParties = MC_ManagementParties.objects.filter(Employee=Employee).values('Party')
+                party_values = [str(record['Party']) for record in EmpParties]
+                if int(Group) > 0:
+                    
+                    
 
+                    PartydetailsOnclusterdata = M_PartyDetails.objects.raw('''select 1 as id, PartyID, PartyName, Group_id, Cluster_id, ClusterName, SubCluster_id, SubClusterName, Supplier_id, SupplierName from 
+(select id PartyID,Name PartyName from M_Parties where PartyType_id in (9,10) and id in %s)a
+left join 
+(select  Party_id,M_PartyDetails.Group_id,M_PartyDetails.Cluster_id,M_Cluster.Name ClusterName,M_PartyDetails.SubCluster_id,M_SubCluster.Name SubClusterName,M_PartyDetails.Supplier_id ,a.Name SupplierName
+from M_PartyDetails 
+LEFT JOIN
+    M_Cluster ON M_PartyDetails.Cluster_id = M_Cluster.id
+        LEFT JOIN
+    M_SubCluster ON M_PartyDetails.SubCluster_id = M_SubCluster.id
+        LEFT JOIN
+    M_Parties a ON a.id = M_PartyDetails.Supplier_id where Group_id = %s)b 
+on a.partyID=b.Party_id ''',(party_values, Group))
+                    
+
+                else:
+                   
+                    PartydetailsOnclusterdata = M_PartyDetails.objects.raw('''select 1 as id, PartyID, PartyName, Group_id, Cluster_id, ClusterName, SubCluster_id, SubClusterName, Supplier_id, SupplierName from 
+(select id PartyID,Name PartyName from M_Parties where PartyType_id in (9,10) and id in %s)a
+left join 
+(select  Party_id,M_PartyDetails.Group_id,M_PartyDetails.Cluster_id,M_Cluster.Name ClusterName,M_PartyDetails.SubCluster_id,M_SubCluster.Name SubClusterName,M_PartyDetails.Supplier_id ,a.Name SupplierName
+from M_PartyDetails 
+LEFT JOIN
+    M_Cluster ON M_PartyDetails.Cluster_id = M_Cluster.id
+        LEFT JOIN
+    M_SubCluster ON M_PartyDetails.SubCluster_id = M_SubCluster.id
+        LEFT JOIN
+    M_Parties a ON a.id = M_PartyDetails.Supplier_id where Group_id IS NULL)b 
+on a.partyID=b.Party_id''',([party_values]))
+                
+                
+                # print(PartydetailsOnclusterdata.query)
+                if not PartydetailsOnclusterdata:
+                  
+                    return JsonResponse({'StatusCode': 404, 'Status': False, 'Message': 'Partydetails Not available', 'Data': []})
+                PartydetailsOncluster_serializer =  GetPartydetailsSerializer(PartydetailsOnclusterdata, many=True).data
+                # print(PartydetailsOncluster_serializer)
+                return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': '', 'Data': PartydetailsOncluster_serializer})
+                
+        except Exception as e:
+            
+            return JsonResponse({'StatusCode': 400, 'Status': False, 'Message': str(e), 'Data': []}) 
+        
   
