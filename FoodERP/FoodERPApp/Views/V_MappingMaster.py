8a99525d560675f20d06f8df6547844332c9b37a
Ankita Dhote <ankitadhote09@gmail.com>
2023-12-20 17:45:00 +0530

    Update V_MappingMaster.py

--- FoodERP/FoodERPApp/Views/V_MappingMaster.py
+++ FoodERP/FoodERPApp/Views/V_MappingMaster.py
@@ -40,7 +40,7 @@ class PartyCustomerMappingView(CreateAPIView):
 as Customer,M_PartyCustomerMappingMaster.MapCustomer, MC_PartyAddress.Address as CustomerAddress, M_Parties.GSTIN, M_Routes.Name as RouteName
 From MC_PartySubParty 
 LEFT join M_PartyCustomerMappingMaster ON M_PartyCustomerMappingMaster.Customer_id = MC_PartySubParty.SubParty_id 
-JOIN M_Parties ON M_Parties.id = MC_PartySubParty.SubParty_id AND MC_PartySubParty.Party_id=%s
+JOIN M_Parties ON M_Parties.id = MC_PartySubParty.SubParty_id AND MC_PartySubParty.Party_id=%s AND M_Parties.IsApprovedParty = 0
 JOIN M_PartyType ON M_PartyType.id=M_Parties.PartyType_id AND M_PartyType.IsRetailer=1
 LEFT JOIN M_Routes ON   MC_PartySubParty.Route_id = M_Routes.id 
 LEFT JOIN MC_PartyAddress ON MC_PartyAddress.Party_id = M_Parties.id AND MC_PartyAddress.IsDefault=1''', ([id]))
