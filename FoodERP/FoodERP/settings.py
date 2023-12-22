ede46e214bb25c064eeabe08a7488f89680b2ab7
pradnyaubale <pradnyaubale12@gmail.com>
2023-12-22 13:27:51 +0530

    Update settings.py

--- FoodERP/FoodERP/settings.py
+++ FoodERP/FoodERP/settings.py
@@ -27,6 +27,7 @@ ALLOWED_HOSTS = ['localhost','10.1.201.19','103.135.203.145','192.168.1.114','11
 CORS_ORIGIN_ALLOW_ALL = True #we allow the all domain to access through API
 # CRONTAB_DJANGO_SETTINGS_MODULE = 'FoodERP.settings'
 INSTALLED_APPS = [
+    'django_crontab',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -36,7 +37,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'FoodERPApp.apps.FooderpappConfig',
-    'django_crontab',
+   
     # 'activity_log',
 ]
 
@@ -105,7 +106,7 @@ DATABASES = {
 }
 
 CRONJOBS = [
-    ('*/30 * * * *', 'FoodERPApp.cron.my_cron_job')
+    ('*/1 * * * *', 'FoodERPApp.cron.my_cron_job')
 ]
 
 # SESSION_ENGINE = 'django.contrib.sessions.backends.db'
