946e04382b04ec4ad7e654e71c393631760a834d
pradnyaubale <pradnyaubale12@gmail.com>
2023-12-26 13:37:29 +0530

    Update urls.py

--- FoodERP/FoodERP/urls.py
+++ FoodERP/FoodERP/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from FoodERPApp.Views.V_Errorhandler import *
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+#    path('admin/', admin.site.urls),
     path('', include('FoodERPApp.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
