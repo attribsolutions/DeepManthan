a82336d81e007f83e6128691e80a5762e251d697
pradnyaubale <pradnyaubale12@gmail.com>
2024-02-07 11:37:35 +0530

    current stock report for all

--- FoodERP/FoodERPApp/Views/V_StockEntry.py
+++ FoodERP/FoodERPApp/Views/V_StockEntry.py
@@ -168,24 +168,16 @@ class ShowOBatchWiseLiveStockView(CreateAPIView):
                         for row in EmpPartys:
                             p=row.id
                         PartyIDs = p.split(",")
-                else : 
-                        PartyID=Party;
-                        PartyIDs= Party  
-                
-                Stockquery='''SELECT M_Parties.id DistributorID,M_Parties.name DistributorName,ifnull(M_GroupType.Name,'') GroupTypeName,
+                        
+                        Stockquery='''SELECT M_Parties.id DistributorID,M_Parties.name DistributorName,ifnull(M_GroupType.Name,'') GroupTypeName,
 ifnull(M_Group.Name,'') GroupName,ifnull(MC_SubGroup.Name,'') SubGroupName, M_Items.id,M_Items.Name,O_BatchWiseLiveStock.Party_id,
-O_BatchWiseLiveStock.BaseUnitQuantity Qty ,
-ifnull((case when IsDamagePieces=0 then O_BatchWiseLiveStock.BaseUnitQuantity end),0)SaleableStock,
-ifnull((case when IsDamagePieces=1 then O_BatchWiseLiveStock.BaseUnitQuantity end),0)UnSaleableStock,
+sum(O_BatchWiseLiveStock.BaseUnitQuantity)Qty ,
+ifnull(sum(case when IsDamagePieces=0 then O_BatchWiseLiveStock.BaseUnitQuantity end),0)SaleableStock,
+ifnull(sum(case when IsDamagePieces=1 then O_BatchWiseLiveStock.BaseUnitQuantity end),0)UnSaleableStock,
 O_LiveBatches.MRPValue ,
-O_LiveBatches.BatchCode,O_LiveBatches.SystemBatchCode,round(GSTHsnCodeMaster(M_Items.id,%s,2),2)GSTPercentage,'''
+0 BatchCode,0 SystemBatchCode,round(GSTHsnCodeMaster(M_Items.id,%s,2),2)GSTPercentage,0 Rate
                 
-                if Party == "":
-                    Stockquery += "0 Rate"
-                else: 
-                    Stockquery += "RateCalculationFunction1(0, M_Items.id, MC_PartyItems.Party_id, %s, 0, 0, O_LiveBatches.MRPValue, 0)Rate"
-
-                Stockquery += ''' FROM M_Items 
+                FROM M_Items 
 join MC_PartyItems on M_Items.id=MC_PartyItems.Item_id and MC_PartyItems.Party_id in %s
 left JOIN MC_ItemGroupDetails ON MC_ItemGroupDetails.Item_id = M_Items.id 
 left JOIN M_GroupType ON M_GroupType.id = MC_ItemGroupDetails.GroupType_id
@@ -194,8 +186,37 @@ left JOIN MC_SubGroup ON MC_SubGroup.id  = MC_ItemGroupDetails.SubGroup_id
 join O_BatchWiseLiveStock on M_Items.id=O_BatchWiseLiveStock.Item_id and O_BatchWiseLiveStock.Party_id= MC_PartyItems.Party_id
 JOIN O_LiveBatches ON O_LiveBatches.id=O_BatchWiseLiveStock.LiveBatche_id
 join M_Parties on M_Parties.id =MC_PartyItems.Party_id
-where O_BatchWiseLiveStock.BaseUnitQuantity >0    order by M_Group.id, MC_SubGroup.id ,M_Items.id,M_Parties.id'''    
+where O_BatchWiseLiveStock.BaseUnitQuantity >0 
+group by M_Parties.id, M_GroupType.id,
+M_Group.id,MC_SubGroup.id, M_Items.id , GSTPercentage,MRPValue
+order by M_Parties.id ,M_Group.id, MC_SubGroup.id ,M_Items.id   '''
                 
+                else : 
+                    PartyID=Party;
+                    PartyIDs= Party  
+                
+                    Stockquery='''SELECT M_Parties.id DistributorID,M_Parties.name DistributorName,ifnull(M_GroupType.Name,'') GroupTypeName,
+    ifnull(M_Group.Name,'') GroupName,ifnull(MC_SubGroup.Name,'') SubGroupName, M_Items.id,M_Items.Name,O_BatchWiseLiveStock.Party_id,
+    O_BatchWiseLiveStock.BaseUnitQuantity Qty ,
+    ifnull((case when IsDamagePieces=0 then O_BatchWiseLiveStock.BaseUnitQuantity end),0)SaleableStock,
+    ifnull((case when IsDamagePieces=1 then O_BatchWiseLiveStock.BaseUnitQuantity end),0)UnSaleableStock,
+    O_LiveBatches.MRPValue ,
+    O_LiveBatches.BatchCode,O_LiveBatches.SystemBatchCode,round(GSTHsnCodeMaster(M_Items.id,%s,2),2)GSTPercentage,
+    RateCalculationFunction1(0, M_Items.id, MC_PartyItems.Party_id, %s, 0, 0, O_LiveBatches.MRPValue, 0)Rate
+    FROM M_Items 
+    join MC_PartyItems on M_Items.id=MC_PartyItems.Item_id and MC_PartyItems.Party_id in %s
+    left JOIN MC_ItemGroupDetails ON MC_ItemGroupDetails.Item_id = M_Items.id 
+    left JOIN M_GroupType ON M_GroupType.id = MC_ItemGroupDetails.GroupType_id
+    left JOIN M_Group ON M_Group.id  = MC_ItemGroupDetails.Group_id 
+    left JOIN MC_SubGroup ON MC_SubGroup.id  = MC_ItemGroupDetails.SubGroup_id
+    join O_BatchWiseLiveStock on M_Items.id=O_BatchWiseLiveStock.Item_id and O_BatchWiseLiveStock.Party_id= MC_PartyItems.Party_id
+    JOIN O_LiveBatches ON O_LiveBatches.id=O_BatchWiseLiveStock.LiveBatche_id
+    join M_Parties on M_Parties.id =MC_PartyItems.Party_id
+    where O_BatchWiseLiveStock.BaseUnitQuantity >0 order by M_Group.id, MC_SubGroup.id ,M_Items.id,M_Parties.id   '''    
+                    
+                    
+                    
+                    
                 if Party == "":
                     Itemquery = MC_PartyItems.objects.raw(Stockquery, (today,PartyIDs))
                 else :
@@ -203,25 +224,30 @@ where O_BatchWiseLiveStock.BaseUnitQuantity >0    order by M_Group.id, MC_SubGro
                 
                 
                 
-#                 Itemquery= MC_PartyItems.objects.raw('''SELECT M_Parties.id DistributorID,M_Parties.name DistributorName,ifnull(M_GroupType.Name,'') GroupTypeName,
-# ifnull(M_Group.Name,'') GroupName,ifnull(MC_SubGroup.Name,'') SubGroupName, M_Items.id,M_Items.Name,O_BatchWiseLiveStock.Party_id,
-# O_BatchWiseLiveStock.BaseUnitQuantity Qty ,
-# ifnull((case when IsDamagePieces=0 then O_BatchWiseLiveStock.BaseUnitQuantity end),0)SaleableStock,
-# ifnull((case when IsDamagePieces=1 then O_BatchWiseLiveStock.BaseUnitQuantity end),0)UnSaleableStock,
-# O_LiveBatches.MRPValue ,
-# O_LiveBatches.BatchCode,O_LiveBatches.SystemBatchCode,RateCalculationFunction1(0, M_Items.id, MC_PartyItems.Party_id, %s, 0, 0, O_LiveBatches.MRPValue, 0)Rate,
-# round(GSTHsnCodeMaster(M_Items.id,%s,2),2)GSTPercentage
-# FROM M_Items 
-# join MC_PartyItems on M_Items.id=MC_PartyItems.Item_id and MC_PartyItems.Party_id in %s
-# left JOIN MC_ItemGroupDetails ON MC_ItemGroupDetails.Item_id = M_Items.id 
-# left JOIN M_GroupType ON M_GroupType.id = MC_ItemGroupDetails.GroupType_id
-# left JOIN M_Group ON M_Group.id  = MC_ItemGroupDetails.Group_id 
-# left JOIN MC_SubGroup ON MC_SubGroup.id  = MC_ItemGroupDetails.SubGroup_id
-# join O_BatchWiseLiveStock on M_Items.id=O_BatchWiseLiveStock.Item_id and O_BatchWiseLiveStock.Party_id= MC_PartyItems.Party_id
-# JOIN O_LiveBatches ON O_LiveBatches.id=O_BatchWiseLiveStock.LiveBatche_id
-# join M_Parties on M_Parties.id =MC_PartyItems.Party_id
-# where O_BatchWiseLiveStock.BaseUnitQuantity >0   order by M_Group.id, MC_SubGroup.id ,M_Items.id ''',(Unit,today,PartyIDs))
-                print(str(Itemquery.query))
+
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                # print(str(Itemquery.query))
+                
                 if not Itemquery:
                     log_entry = create_transaction_logNew(request, StockReportdata, Party, "BatchWiseLiveStock Not available",88,0) 
                     return JsonResponse({'StatusCode': 204, 'Status': True, 'Message':  'Items Not available', 'Data': []})
