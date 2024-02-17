5dad5067811bd8226869e3b64931ec9699c0ed00
pradnyaubale <pradnyaubale12@gmail.com>
2024-02-17 11:11:03 +0530

    Update V_SweetPosRoleAccess.py

--- FoodERP/SweetPOS/Views/V_SweetPosRoleAccess.py
+++ FoodERP/SweetPOS/Views/V_SweetPosRoleAccess.py
@@ -11,6 +11,7 @@ from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework.authentication import BasicAuthentication
 import pdb
+from FoodERPApp.Views.V_CommFunction import create_transaction_logNew
 
 
 def BasicAuthenticationfunction(request):
@@ -55,9 +56,10 @@ class SweetPosRoleAccessView(CreateAPIView):
                         obj = M_SweetPOSRoleAccess(**data)
                         obj.save(using='sweetpos_db')
                         
-                    
+                # log_entry = create_transaction_logNew(request, SPOSRoleAccessdata,SPOSRoleAccessdata['Party'],'',346,0)    
                 return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'SweetPosRoleAccess Save Successfully', 'Data':[]}) 
         except Exception as e:
+            # log_entry = create_transaction_logNew(request, 0,0,'SweetPosRoleAccess Save:'+str(list(e)),33,0)
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  list(e), 'Data':[]})
         
     @transaction.atomic()
@@ -73,11 +75,13 @@ class SweetPosRoleAccessView(CreateAPIView):
                                         
                 RoleAccess_serializer = SPOSRoleAccessSerializerSecond(RoleAccessData, many=True).data
                 
-                
+                log_entry = create_transaction_logNew(request, RoleAccess_serializer,0,'',347,0)
                 return JsonResponse({'StatusCode': 200, 'Status': True, 'Message':'','Data': RoleAccess_serializer})
         except  M_SweetPOSRoleAccess.DoesNotExist:
+            log_entry = create_transaction_logNew(request, 0,0,'GETSweetPOSRoleAccessDetails'+'RoleAccess Not available',347,0)
             return JsonResponse({'StatusCode': 204, 'Status': True,'Message':  'RoleAccess Not available', 'Data': []})
         except Exception as e:
+            log_entry = create_transaction_logNew(request, 0,0,'GETSweetPOSRoleAccessDetails:'+str(Exception(e)),33,0)
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  Exception(e), 'Data':[]})
 
 
@@ -106,10 +110,12 @@ class SPOSLog_inView(CreateAPIView):
                     #     instance.save(using='sweetpos_db')
                     responce=M_SweetPOSRoleAccess.objects.using('sweetpos_db').get(DivisionID=Division)
                     responce_serializer=SPOSRoleAccessSerializer(responce).data
-
+                    
+                    log_entry = create_transaction_logNew(request, SPOSLog_in_data,0,'',348,0)
                     return JsonResponse({"Success":True,"status_code":200,"msg":"Loged In Successfully..!","RoleAccess": responce_serializer})
                     # else:
                     #     transaction.set_rollback(True)
                     #     return JsonResponse({'StatusCode': 406, 'Status': True, 'Message': SPOSLog_in_data_serializer.errors, 'Data': []})
         except Exception as e:
+            log_entry = create_transaction_logNew(request, 0,0,'SweetPOSLogin:'+str(Exception(e)),33,0)
             raise JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  Exception(e), 'Data':[]})
