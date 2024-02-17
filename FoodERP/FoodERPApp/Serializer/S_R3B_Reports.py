683f38146cc1e5b38362e795762d427049ec8b1e
PratikshaYadav19 <pratikshayadav222.com>
2024-02-17 12:18:16 +0530

    R3B report
    Change labels

--- FoodERP/FoodERPApp/Serializer/S_R3B_Reports.py
+++ FoodERP/FoodERPApp/Serializer/S_R3B_Reports.py
@@ -9,15 +9,24 @@ class FloatDecimalField(serializers.Field):
     
 '''Details of Outward Supplies and inward supplies liable to reverse charge'''
 class DOSAISLTRCSerializer(serializers.Serializer):
-    # id = serializers.IntegerField()
     NatureOfSupplies = serializers.CharField(max_length=100)
-    TotalTaxableValue=FloatDecimalField()
-    IntegratedTax=FloatDecimalField()
-    CentralTax=FloatDecimalField()
-    State_UTTax=FloatDecimalField()
-    Cess =FloatDecimalField()
+    TotalTaxableValue = FloatDecimalField()
+    IntegratedTax = FloatDecimalField()
+    CentralTax = FloatDecimalField()
+    State_UTTax = FloatDecimalField()
+    Cess = FloatDecimalField()
     
-    
+    def to_representation(self, obj):
+        representation = super().to_representation(obj)
+        representation['Nature Of Supplies'] = representation.pop('NatureOfSupplies')
+        representation['Total Taxable Value'] = representation.pop('TotalTaxableValue')
+        representation['Integrated Tax'] = representation.pop('IntegratedTax')
+        representation['Central Tax'] = representation.pop('CentralTax')
+        representation['State / UT Tax'] = representation.pop('State_UTTax')
+        representation['Cess'] = representation.pop('Cess')
+        return representation
+
+   
 class EligibleITCSerializer(serializers.Serializer):
     # id = serializers.IntegerField()
     Details = serializers.CharField(max_length=100)
@@ -25,6 +34,15 @@ class EligibleITCSerializer(serializers.Serializer):
     CentralTax=FloatDecimalField()
     State_UTTax=FloatDecimalField()
     Cess=FloatDecimalField()
+    
+    def to_representation(self, obj):
+        representation = super().to_representation(obj)
+        representation['Details'] = representation.pop('Details')
+        representation['Integrated Tax'] = representation.pop('IntegratedTax')
+        representation['Central Tax'] = representation.pop('CentralTax')
+        representation['State / UT Tax'] = representation.pop('State_UTTax')
+        representation['Cess'] = representation.pop('Cess')
+        return representation
 
 
 class Query3Serializer (serializers.Serializer):
@@ -32,4 +50,10 @@ class Query3Serializer (serializers.Serializer):
     PlaceOfSupplyState_UT = serializers.CharField(max_length=100)
     TaxableValue=FloatDecimalField()
     AmountOfIntegratedTax=FloatDecimalField()
-     
+    
+    def to_representation(self, obj):
+        representation = super().to_representation(obj)
+        representation ['Place Of Supply State / UT'] = representation.pop('PlaceOfSupplyState_UT')
+        representation ['Taxable Value'] = representation.pop('TaxableValue')
+        representation ['Amount Of Integrated Tax'] = representation.pop('AmountOfIntegratedTax')
+        return representation
