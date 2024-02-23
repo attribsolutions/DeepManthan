d4cfc81328c1db99f4ca6061d1faa291f23d5bdb
PratikshaYadav19 <pratikshayadav222.com>
2024-02-23 15:33:43 +0530

    Update urls.py
    ItemList url

--- FoodERP/SweetPOS/urls.py
+++ FoodERP/SweetPOS/urls.py
@@ -4,12 +4,17 @@ from .Views.V_SweetPosRoleAccess import *
 
 from .Views.V_SweetPoSItemGroup import *
 
+
 urlpatterns = [
     
     # Master APIs IN Projects Add Page ,List Page
     url(r'SPOSroleaccess$', SweetPosRoleAccessView.as_view()),
     url(r'Lspos$', SPOSLog_inView.as_view()),
-    url(r'ItemGroupandSubgroup$', ItemGroupandSubgroupView.as_view())
+    url(r'ItemGroupandSubgroup$', ItemGroupandSubgroupView.as_view()),
+    url(r'ItemList$', ItemListView.as_view())
+        
+    
+    
     
     
 ]
