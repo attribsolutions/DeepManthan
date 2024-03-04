a83eb7be6fe4c660c40a3f3a73080e3ef9d48ec5
PratikshaYadav19 <pratikshayadav222.com>
2024-03-04 14:45:38 +0530

    GetTargetUpload
    GET API for sheet

--- FoodERP/FoodERPApp/Serializer/S_TargetUploads.py
+++ FoodERP/FoodERPApp/Serializer/S_TargetUploads.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 from ..models import *
 from datetime import datetime
 
+class PartySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = M_Parties
+        fields = '__all__'
+
 class TargetUploadsSerializer(serializers.ModelSerializer):
+    Party = PartySerializer(read_only=True)
     class Meta:
         model = T_TargetUploads
         fields = '__all__'
+
+
