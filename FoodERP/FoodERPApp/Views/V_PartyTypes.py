cb546b40cf30e1b5c56e23c88dbfe91ba662f7ef
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2024-03-11 18:04:41 +0530

    Update V_PartyTypes.py

--- FoodERP/FoodERPApp/Views/V_PartyTypes.py
+++ FoodERP/FoodERPApp/Views/V_PartyTypes.py
@@ -36,11 +36,11 @@ class PartyTypeListView(CreateAPIView):
                         if(IsSCM == 0):
                         
                             q= C_Companies.objects.filter(id=CompanyID).values("CompanyGroup")
-                            print(str(q.query))
+                            # print(str(q.query))
                             q0=C_Companies.objects.filter(IsSCM=IsSCM,CompanyGroup=q[0]['CompanyGroup']).values('id')
-                            print(str(q0.query))
+                            # print(str(q0.query))
                             query = M_PartyType.objects.filter(Q(Company=CompanyID  ) | Q(Company=q0[0]['id']))
-                            print(str(query.query))
+                            # print(str(query.query))
                             p=0
                         else:
                         
