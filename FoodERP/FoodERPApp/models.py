e46d929d863928a6e4a27ade3be5d3816a632228
pradnyaubale <pradnyaubale12@gmail.com>
2024-03-28 10:21:22 +0530

    New Database Table M_FinancialYearFirstTransactionLog

--- FoodERP/FoodERPApp/models.py
+++ FoodERP/FoodERPApp/models.py
@@ -2294,3 +2294,10 @@ class T_TargetUploads(models.Model):
 
 
 	
+class M_FinancialYearFirstTransactionLog(models.Model):
+    FinancialYear = models.IntegerField(default=False)
+    Party = models.IntegerField(default=False)
+    Flag =  models.BooleanField(default=False) 
+
+    class Meta:
+        db_table = "M_FinancialYearFirstTransactionLog"
