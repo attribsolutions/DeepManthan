6596e21feefa3d7f4946d8fda5566619c029d5fa
PratikshaYadav19 <pratikshayadav222.com>
2024-04-16 15:45:27 +0530

    V_SweetPoSUsers
    Add RoleName

--- FoodERP/SweetPOS/Serializer/S_SweetPoSUsers.py
+++ FoodERP/SweetPOS/Serializer/S_SweetPoSUsers.py
@@ -1,15 +1,24 @@
 from ..models import *
 from rest_framework import serializers
 
-
-class UsersSerializer(serializers.ModelSerializer):
-    class Meta :
-        model= M_SweetPOSUser
-        fields = '__all__'
-        
 class RolesSerializer(serializers.ModelSerializer):
-    class Meta :
-        model= M_SweetPOSRoles
-        fields = '__all__'
-        
-        
+    class Meta:
+        model = M_SweetPOSRoles
+        fields ="__all__"
+
+class UsersSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    CompanyID = serializers.IntegerField()
+    DivisionID = serializers.IntegerField()
+    LoginName = serializers.CharField(max_length=500)
+    Password = serializers.CharField(max_length=500)
+    RoleID = serializers.IntegerField()
+    IsActive =  serializers.IntegerField()
+    CreatedBy = serializers.CharField(max_length=500)
+    CreatedOn = serializers.CharField(max_length=500)
+    UpdatedBy = serializers.CharField(max_length=500)
+    UpdatedOn = serializers.CharField(max_length=500)
+    RoleName = serializers.CharField(max_length=500)
+
+
+
