5a1d0844764299c7ca53646af56ad2d442fb92b9
Ankita Dhote <ankitadhote09@gmail.com>
2024-04-24 15:47:53 +0530

    Update V_CompanyGroup.py

--- FoodERP/FoodERPApp/Views/V_CompanyGroup.py
+++ FoodERP/FoodERPApp/Views/V_CompanyGroup.py
@@ -73,20 +73,23 @@ class C_CompanyGroupViewSecond(CreateAPIView):
 
     @transaction.atomic()
     def put(self, request, id=0):
+        CompaniesGropusdata = JSONParser().parse(request)
         try:
             with transaction.atomic():
-                CompaniesGropusdata = JSONParser().parse(request)
                 CompaniesGropusdataByID = C_CompanyGroups.objects.get(id=id)
                 CompaniesGropus_Serializer = C_CompanyGroupSerializer(
                     CompaniesGropusdataByID, data=CompaniesGropusdata)
                 if CompaniesGropus_Serializer.is_valid():
                     CompaniesGropus_Serializer.save()
+                    log_entry = create_transaction_logNew(request, CompaniesGropusdata,0,'',316,0)
                     return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'Company Group Updated Successfully', 'Data':[]})
                 else:
+                    log_entry = create_transaction_logNew(request, CompaniesGropusdata,0,'CompanyGroupUpdate:'+ str(CompaniesGropus_Serializer.errors),34,0)
                     transaction.set_rollback(True)
                     return JsonResponse({'StatusCode': 406, 'Status': True, 'Message': CompaniesGropus_Serializer.errors, 'Data':[]})
         except Exception as e:
-            return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  Exception(e), 'Data':[]})
+            log_entry = create_transaction_logNew(request, CompaniesGropusdata,0,'CompanyGroupUpdate:'+str(e),33,0)
+            return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  str(e), 'Data':[]})
            
 
     @transaction.atomic()
@@ -95,8 +98,11 @@ class C_CompanyGroupViewSecond(CreateAPIView):
             with transaction.atomic():
                 CompaniesGropusdata = C_CompanyGroups.objects.get(id=id)
                 CompaniesGropusdata.delete()
+                log_entry = create_transaction_logNew(request, {'CompanyID':id},0,'',317,0)
                 return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'Company Group  Deleted Successfully', 'Data':[]})
         except C_CompanyGroups.DoesNotExist:
+            log_entry = create_transaction_logNew(request, {'CompanyID':id},0,'',317,0)
             return JsonResponse({'StatusCode': 204, 'Status': True, 'Message':'Company Group Not available', 'Data': []})
         except IntegrityError:   
+            log_entry = create_transaction_logNew(request, {'CompanyID':id},0,'',8,0)
             return JsonResponse({'StatusCode': 204, 'Status': True, 'Message':'Company Group used in another table', 'Data': []})  
