6c1e6fd196b21c6cbb3d749a3cb8fac4a6d2d602
PratikshaYadav19 <pratikshayadav222.com>
2024-05-07 17:32:00 +0530

    Update V_Discount.py
    to cross check

--- FoodERP/FoodERPApp/Views/V_Discount.py
+++ FoodERP/FoodERPApp/Views/V_Discount.py
@@ -121,6 +121,7 @@ class DiscountMasterSaveView(CreateAPIView):
         DiscountMaster_data = JSONParser().parse(request)
         try:
             with transaction.atomic():
+                DiscountMaster_data = JSONParser().parse(request)
                 Discount_serializer = DiscountSerializer(data=DiscountMaster_data, many=True)
                 if Discount_serializer.is_valid():
                     Discount = Discount_serializer.save()
