29ae7acf97651d42a29e74b706cecd14fb8d44c4
hemantwaghmare <79359038+hemantwaghmare@users.noreply.github.com>
2024-05-28 12:37:44 +0530

    V_StockEntry

--- FoodERP/FoodERPApp/models.py
+++ FoodERP/FoodERPApp/models.py
@@ -1273,7 +1273,7 @@ class T_Production(models.Model):
         StoreLocation = models.CharField(max_length=500)
         PrintedBatchCode = models.CharField(max_length=500)		
         BestBefore = models.DateField()  
-        Remark = models.CharField(max_length=500)	
+        Remark = models.CharField(max_length=500,null=True,blank=True)	
         CreatedBy = models.IntegerField()
         CreatedOn = models.DateTimeField(auto_now_add=True)
         UpdatedBy = models.IntegerField()
