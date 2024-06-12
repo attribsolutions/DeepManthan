fdac4cbfcc9003055768630dd05bd8a2d747375e
pradnyaubale <pradnyaubale12@gmail.com>
2024-06-12 12:25:51 +0530

    OrderEdit API Changes for Demand Edit Fun

--- FoodERP/FoodERPApp/Serializer/S_Orders.py
+++ FoodERP/FoodERPApp/Serializer/S_Orders.py
@@ -347,11 +347,11 @@ class OrderEditserializer(serializers.Serializer):
     DiscountAmount = serializers.DecimalField(max_digits=20, decimal_places=2)
     StockQuantity = serializers.DecimalField(max_digits=20, decimal_places=3)
     
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        if data['StockQuantity'] is None:
-            data['StockQuantity'] = '0.00'
-        return data
+    # def to_representation(self, instance):
+    #     data = super().to_representation(instance)
+    #     if data['StockQuantity'] is None:
+    #         data['StockQuantity'] = '0.00'
+    #     return data
    
 
 class ReportOrderItemSerializer(serializers.ModelSerializer):
