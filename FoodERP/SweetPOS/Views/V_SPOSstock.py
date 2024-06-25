ea7650420f474ac6aa2447eae2b97354bc6701cf
Ankita Dhote <ankitadhote09@gmail.com>
2024-06-25 12:04:19 +0530

    SPOSstock Report

--- FoodERP/SweetPOS/Views/V_SPOSstock.py
+++ FoodERP/SweetPOS/Views/V_SPOSstock.py
@@ -9,10 +9,11 @@ from FoodERPApp.models import *
 from FoodERPApp.Serializer.S_PartyItems import *
 from FoodERPApp.Serializer.S_Orders import *
 from ..models import  *
-from SweetPOS.Serializer.S_SPOSstock import SPOSstockSerializer
+from SweetPOS.Serializer.S_SPOSstock import SPOSstockSerializer, SPOSStockReportSerializer
 from django.db.models import *
 from FoodERPApp.Views.V_TransactionNumberfun import SystemBatchCodeGeneration
 from datetime import date
+from FoodERPApp.models import * 
 
 class StockView(CreateAPIView):
     permission_classes = (IsAuthenticated,)
@@ -81,3 +82,78 @@ class StockView(CreateAPIView):
         except Exception as e:
             log_entry = create_transaction_logNew(request, FranchiseStockdata, 0,'FranchiseStockEntrySave:'+str(e),33,0)
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':Exception(e), 'Data': []})
+
+
+
+class SPOSStockReportView(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, id=0):
+        Orderdata = JSONParser().parse(request)
+        try:
+            with transaction.atomic():
+                FromDate = Orderdata['FromDate']
+                ToDate = Orderdata['ToDate']
+                Unit = Orderdata['Unit']
+                Party = Orderdata['Party']
+                PartyNameQ = M_Parties.objects.filter(id=Party).values("Name")
+                UnitName = M_Units.objects.filter(id=Unit).values("Name")
+                unitname = UnitName[0]['Name']
+                StockreportQuery = O_SPOSDateWiseLiveStock.objects.raw('''SELECT 1 as id,A.Item_id,A.Unit,
+fooderp20240624.UnitwiseQuantityConversion(A.Item_id,ifnull(OpeningBalance,0),0,A.Unit,0,%s,0)OpeningBalance,
+fooderp20240624.UnitwiseQuantityConversion(A.Item_id,GRNInward,0,A.Unit,0,%s,0)GRNInward,
+fooderp20240624.UnitwiseQuantityConversion(A.Item_id,Sale,0,A.Unit,0,%s,0)Sale,
+fooderp20240624.UnitwiseQuantityConversion(A.Item_id,ClosingBalance,0,A.Unit,0,%s,0)ClosingBalance,
+fooderp20240624.UnitwiseQuantityConversion(A.Item_id,ActualStock,0,A.Unit,0,%s,0)ActualStock,
+A.ItemName,
+D.QuantityInBaseUnit,
+fooderp20240624.UnitwiseQuantityConversion(A.Item_id,PurchaseReturn,0,A.Unit,0,%s,0)PurchaseReturn,
+fooderp20240624.UnitwiseQuantityConversion(A.Item_id,SalesReturn,0,A.Unit,0,%s,0)SalesReturn,
+fooderp20240624.UnitwiseQuantityConversion(A.Item_id,StockAdjustment,0,A.Unit,0,%s,0)StockAdjustment
+,GroupTypeName,GroupName,SubGroupName,%s UnitName
+FROM
+
+        ( SELECT M_Items.id Item_id, M_Items.Name ItemName ,Unit,M_Units.Name UnitName ,SUM(GRN) GRNInward, SUM(Sale) Sale, SUM(PurchaseReturn)PurchaseReturn,SUM(SalesReturn)SalesReturn,SUM(StockAdjustment)StockAdjustment,
+    ifnull(M_GroupType.Name,'') GroupTypeName,ifnull(M_Group.Name,'') GroupName,ifnull(MC_SubGroup.Name,'') SubGroupName
+         FROM O_SPOSDateWiseLiveStock
+
+            JOIN fooderp20240624.M_Items ON M_Items.id=O_SPOSDateWiseLiveStock.Item
+        join fooderp20240624.M_Units on M_Units.id= O_SPOSDateWiseLiveStock.Unit
+        left join fooderp20240624.MC_ItemGroupDetails on MC_ItemGroupDetails.Item_id=M_Items.id
+                left JOIN fooderp20240624.M_GroupType ON M_GroupType.id = MC_ItemGroupDetails.GroupType_id
+                left JOIN fooderp20240624.M_Group ON M_Group.id  = MC_ItemGroupDetails.Group_id
+                left JOIN fooderp20240624.MC_SubGroup ON MC_SubGroup.id  = MC_ItemGroupDetails.SubGroup_id
+                 WHERE StockDate BETWEEN %s AND %s AND Party=%s GROUP BY Item,M_GroupType.id,M_Group.id,MC_SubGroup.id) A
+
+                left JOIN (SELECT O_SPOSDateWiseLiveStock.Item, OpeningBalance FROM O_SPOSDateWiseLiveStock WHERE O_SPOSDateWiseLiveStock.StockDate = %s AND O_SPOSDateWiseLiveStock.Party=%s) B
+
+                 ON A.Item_id = B.Item
+
+                 left JOIN (SELECT Item, ClosingBalance, ActualStock FROM O_SPOSDateWiseLiveStock WHERE StockDate = %s AND Party=%s) C 
+
+                  ON A.Item_id = C.Item
+
+                LEFT JOIN (SELECT Item, SUM(BaseunitQuantity) QuantityInBaseUnit
+                FROM T_SPOSStock
+                WHERE Party =%s AND StockDate BETWEEN %s AND %s
+                GROUP BY Item) D
+                ON A.Item_id = D.Item ''', ([Unit], [Unit], [Unit], [Unit], [Unit], [Unit], [Unit], [Unit], [unitname], [FromDate], [ToDate], [Party], [FromDate], [Party], [ToDate], [Party], [Party], [FromDate], [ToDate]))
+                # CustomPrint(StockreportQuery)
+                serializer = SPOSStockReportSerializer(StockreportQuery, many=True).data
+
+                StockData = list()
+                StockData.append({
+                    "FromDate": FromDate,
+                    "ToDate": ToDate,
+                    "PartyName": PartyNameQ[0]["Name"],
+                    "StockDetails": serializer})
+
+                if StockreportQuery:
+                    log_entry = create_transaction_logNew(request, Orderdata, Party, 'From:'+str(FromDate)+','+'To:'+str(ToDate), 210, 0, FromDate, ToDate, 0)
+                    return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': '', 'Data': StockData})
+                else:
+                    log_entry = create_transaction_logNew(request, Orderdata, Party, 'Recort Not Found', 210, 0)
+                    return JsonResponse({'StatusCode': 204, 'Status': True, 'Message': 'Record Not Found', 'Data': []})
+        except Exception as e:
+            log_entry = create_transaction_logNew(request,Orderdata, 0, 'StockReport:'+str(e), 33, 0)
+            return JsonResponse({'StatusCode': 400, 'Status': True, 'Message': Exception(e), 'Data': []})
