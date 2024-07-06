78db547159dc94eaf4a7299cda895d4e49c7dcd9
Ankita Dhote <ankitadhote09@gmail.com>
2024-07-06 14:24:30 +0530

    changes

--- FoodERP/FoodERPApp/Views/V_Invoices.py
+++ FoodERP/FoodERPApp/Views/V_Invoices.py
@@ -17,7 +17,7 @@ from django.shortcuts import get_object_or_404
 from django.http import Http404 
 from SweetPOS.models import *
 from django.db.models import F
-
+from SweetPOS.models import *
 
 class OrderDetailsForInvoice(CreateAPIView):
     
@@ -324,7 +324,7 @@ class InvoiceListFilterViewSecond(CreateAPIView):
                         
                 InvoiceListData = list()
                 for a in combined_invoices:
-                        q=TC_InvoiceUploads.objects.filter(Invoice=a["id"])
+                        q=TC_SPOSInvoiceUploads.objects.filter(Invoice=a["id"])
                         Invoice_serializer = SPOSInvoiceSerializer(q, many=True).data
                         if (Invoicedata['DashBoardMode'] == 1):
                             InvoiceListData.append({
