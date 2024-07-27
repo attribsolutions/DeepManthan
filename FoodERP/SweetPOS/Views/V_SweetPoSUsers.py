3d7eb38f911148cef5e667e8f69da05a43df1b44
PratikshaYadav19 <pratikshayadav222.com>
2024-07-27 16:37:40 +0530

    Update V_SweetPoSUsers.py
    remove authentication and commented logs

--- FoodERP/SweetPOS/Views/V_SweetPoSUsers.py
+++ FoodERP/SweetPOS/Views/V_SweetPoSUsers.py
@@ -8,13 +8,10 @@ from rest_framework.parsers import JSONParser
 from rest_framework import status
 from rest_framework.response import Response
 from django.db import IntegrityError, transaction
-from django.contrib.auth import authenticate
-from rest_framework.authentication import BasicAuthentication
-from SweetPOS.Views.V_SweetPosRoleAccess import BasicAuthenticationfunction
 from FoodERPApp.models import *
 from ..Serializer.S_SweetPoSUsers import *
 from FoodERPApp.Views.V_CommFunction import create_transaction_logNew
-from rest_framework.authentication import get_authorization_header, BasicAuthentication, TokenAuthentication
+
 
 
 class SweetPOSUsersView(CreateAPIView):
@@ -76,24 +73,13 @@ class SweetPOSUsersView(CreateAPIView):
             log_entry = create_transaction_logNew(request, 0, 0, 'GETAllUsers:' + str(e), 33, 0)
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message': str(e), 'Data': []})
        
+# Don't CommentOut below Logs without Permission  -- Commented By Pratiksha
 
 class SweetPOSUsersSecondView(CreateAPIView):
-    permission_classes = [IsAuthenticated]
-
-    def authenticate_user(self, request):
-        auth_header = get_authorization_header(request).split()
-        if not auth_header:
-            return None
-
-        if auth_header[0].lower() == b'basic':
-            return BasicAuthentication().authenticate(request)
-        elif auth_header[0].lower() == b'Token':
-            return TokenAuthentication().authenticate(request)
-        return None
+    permission_classes = ()
 
     @transaction.atomic()
     def get(self, request, Division_id=0):
-
         try:
             with transaction.atomic():
                 query = M_SweetPOSUser.objects.raw(f""" SELECT SU.id, CompanyID, DivisionID, LoginName, Password, RoleID, IsActive, SU.CreatedBy, SU.CreatedOn, SU.UpdatedBy, SU.UpdatedOn, M_SweetPOSRoles.Name as RoleName
@@ -118,13 +104,13 @@ class SweetPOSUsersSecondView(CreateAPIView):
                         "RoleName": row.RoleName,
                     })
 
-                log_entry = create_transaction_logNew(request, user_list, 0, '', 374, 0)
+                # log_entry = create_transaction_logNew(request, user_list, 0, '', 374, 0)
                 return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': '', 'Data': user_list})
         except M_SweetPOSUser.DoesNotExist:
-            log_entry = create_transaction_logNew(request, 0, 0, 'User data not available', 374, 0)
+            # log_entry = create_transaction_logNew(request, 0, 0, 'User data not available', 374, 0)
             return JsonResponse({'StatusCode': 204, 'Status': True, 'Message': 'User data not available', 'Data': []})
         except Exception as e:
-            log_entry = create_transaction_logNew(request, 0, 0, 'GETUser:' + str(e), 33, 0)
+            # log_entry = create_transaction_logNew(request, 0, 0, 'GETUser:' + str(e), 33, 0)
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message': str(e), 'Data': []})
 
 
@@ -138,14 +124,14 @@ class SweetPOSUsersSecondView(CreateAPIView):
                 User_data_serializer = UsersSerializer(User_DataByID , data=User_data)
                 if User_data_serializer.is_valid():
                     User_data_serializer.save()
-                    log_entry = create_transaction_logNew(request, User_data,0,'',375,0)
+                    # log_entry = create_transaction_logNew(request, User_data,0,'',375,0)
                     return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'User Data Updated Successfully','Data' :[]})
                 else:
-                    log_entry = create_transaction_logNew(request, User_data,0,'UserEdit:'+str(User_data_serializer.errors),34,0)
+                    # log_entry = create_transaction_logNew(request, User_data,0,'UserEdit:'+str(User_data_serializer.errors),34,0)
                     transaction.set_rollback(True)
                     return JsonResponse({'StatusCode': 406, 'Status': True, 'Message': User_data_serializer.errors, 'Data' :[]})
         except Exception as e:
-            log_entry = create_transaction_logNew(request, User_data, 0,'UserEdit:'+str(e),33,0)
+            # log_entry = create_transaction_logNew(request, User_data, 0,'UserEdit:'+str(e),33,0)
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  str(e), 'Data':[]})
         
         
@@ -155,16 +141,16 @@ class SweetPOSUsersSecondView(CreateAPIView):
             with transaction.atomic():
                 User_data = M_SweetPOSUser.objects.get(id=id)
                 User_data.delete()
-                log_entry = create_transaction_logNew(request, User_data,0,'',376,0)
+                # log_entry = create_transaction_logNew(request, User_data,0,'',376,0)
                 return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'User Data Deleted Successfully','Data':[]})
         except M_SweetPOSUser.DoesNotExist:
-            log_entry = create_transaction_logNew(request, 0,0,'User Data Not available',376,0)
+            # log_entry = create_transaction_logNew(request, 0,0,'User Data Not available',376,0)
             return JsonResponse({'StatusCode': 204, 'Status': True, 'Message':'User Data Not available', 'Data': []})
         except IntegrityError:
-            log_entry = create_transaction_logNew(request, 0,0,'User Data used in another table',8,0)  
+            # log_entry = create_transaction_logNew(request, 0,0,'User Data used in another table',8,0)  
             return JsonResponse({'StatusCode': 204, 'Status': True, 'Message':'User Data used in another table', 'Data': []})
         except Exception as e:
-            log_entry = create_transaction_logNew(request, 0, 0,'UserDelete:'+str(e),33,0)
+            # log_entry = create_transaction_logNew(request, 0, 0,'UserDelete:'+str(e),33,0)
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  str(e), 'Data':[]})  
 
 
