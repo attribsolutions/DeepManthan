009b4b79381a4e726a0e7d55b805878027de60f0
Tejaswini <140879481+Tejaswinisyadav@users.noreply.github.com>
2024-08-01 18:00:11 +0530

    Update CBMItem

--- FoodERP/FoodERPApp/Serializer/S_Items.py
+++ FoodERP/FoodERPApp/Serializer/S_Items.py
@@ -178,7 +178,8 @@ class ItemSerializer(serializers.ModelSerializer):
             'StoringCondition', instance.StoringCondition)
         instance.Grammage = validated_data.get(
             'Grammage', instance.Grammage)
-        instance.Grammage=validated_data.get('IsCBMItem',instance.IsCBMItem)
+        instance.IsCBMItem=validated_data.get(
+            'IsCBMItem',instance.IsCBMItem)
             
         instance.save()
         
