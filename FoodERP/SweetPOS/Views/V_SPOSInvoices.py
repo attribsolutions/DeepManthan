6d05d356d712fc8dcc5a35b051b4274e877ed2e7
Ankita Dhote <ankitadhote09@gmail.com>
2024-10-01 12:08:29 +0530

    Update V_SPOSInvoices.py

--- FoodERP/SweetPOS/Views/V_SPOSInvoices.py
+++ FoodERP/SweetPOS/Views/V_SPOSInvoices.py
@@ -100,9 +100,9 @@ class SPOSInvoiceView(CreateAPIView):
                             
                             InvoiceItem['Unit'] = quryforunit[0]['id']
 
-                            queryformobile = M_ConsumerMobile.objects.filter(Mobile=Invoicedata['MobileNo']).values('Mobile')
+                            queryformobile = M_ConsumerMobile.objects.filter(Mobile=Invoicedata['MobileNo']).values('Mobile','MacID')
                             if queryformobile:
-                                q = M_ConsumerMobile.objects.filter(Mobile=queryformobile[0]['Mobile']).update(IsLinkToBill=1)
+                                q = M_ConsumerMobile.objects.filter(Mobile=queryformobile[0]['Mobile'],MacID=queryformobile[0]['MacID']).update(IsLinkToBill=1)
                             
                             # InvoiceItem['BasicAmount'] = InvoiceItem['Amount']
                             InvoiceItem['InvoiceDate'] = InvoiceItem['SaleDate']
