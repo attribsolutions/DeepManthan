b1290b602d638b5fe977248e29b37b8ffa12a1c8
Ankita Dhote <ankitadhote09@gmail.com>
2024-10-07 15:26:25 +0530

    Update S_PartyTypes.py

--- FoodERP/FoodERPApp/Serializer/S_PartyTypes.py
+++ FoodERP/FoodERPApp/Serializer/S_PartyTypes.py
@@ -12,11 +12,19 @@ class PartyTypeSerializer(serializers.ModelSerializer):
 class PartTypeSerializerSecond(serializers.ModelSerializer):
     SAPIndicator = serializers.CharField(required=False)
     Country = CountrySerializer()
+
     class Meta:
         model = M_PartyType
         fields = ['id','Name', 'IsVendor', 'IsSCM', 'IsDivision', 'IsAdminDivision', 'CreatedBy', 'CreatedOn', 'UpdatedBy', 'UpdatedOn', 'IsRetailer', 'Company', 'IsFranchises', 'SAPIndicator', 'Country']   
 
-
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        
+        country_data = representation.pop('Country', None)
+        if country_data:
+            representation['CountryID'] = country_data.get('id')
+            representation['CountryName'] = country_data.get('Country')
+        return representation
        
 
    
