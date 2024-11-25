a9e20a2030cef88d205c9e861995af154d463406
PratikshaYadav19 <pratikshayadav222.com>
2024-11-25 17:34:01 +0530

    S_Login
    Add flag condition -> IsLoginPermissions

--- FoodERP/FoodERPApp/DatabaseFunctions/GetTodaysDateMRP.py
+++ FoodERP/FoodERPApp/DatabaseFunctions/GetTodaysDateMRP.py
@@ -20,7 +20,7 @@ RETURN result;
 END'''
 
 
-# NEW VERSION OF FUNCTION
+# NEW VERSION OF FUNCTION -> Changes for Chitale Americas -> add PartyType condition
 
 '''CREATE DEFINER=`pk`@`%` FUNCTION `GetTodaysDateMRP`(ItemID int,EffectiveDate1 Date,Modee int ,DivisionID int,PartyID int,PartyTypeID int) RETURNS varchar(200) CHARSET utf8mb4
     READS SQL DATA
@@ -31,6 +31,7 @@ BEGIN
     DECLARE result varchar(50);
 	declare PT_ID int(10);
     declare PartyType int(10);
+    declare EffectiveDatee date;
         
 		if PartyID > 0 then 
 			select PartyType_id into PT_ID from M_Parties where id=PartyID;
@@ -39,29 +40,32 @@ BEGIN
 			set PartyType = PartyTypeID;
         end if;
         
-       
+        
         if PartyID > 0 then
-			SELECT id, MRP into IDD,MRPP FROM M_MRPMaster where Item_id=ItemID
-            and EffectiveDate<=EffectiveDate1 and Party_id=PartyID and PartyType_id is null order by EffectiveDate desc, id desc limit 1;
-		end if;    
+			SELECT id, MRP,EffectiveDate into IDD,MRPP,EffectiveDatee FROM M_MRPMaster where Item_id=ItemID
+            and EffectiveDate<=EffectiveDate1 and Party_id=PartyID and PartyType_id is null and IsDeleted=0 order by EffectiveDate desc, id desc limit 1;
+		end if;  
+        
         if MRPP is null  then
             
 			if PartyType > 0 then
-				SELECT id, MRP into IDD,MRPP FROM M_MRPMaster where Item_id=ItemID
-				and EffectiveDate<=EffectiveDate1 and Party_id is null and PartyType_id = PartyType order by EffectiveDate desc, id desc limit 1;	
+				SELECT id, MRP, EffectiveDate into IDD,MRPP,EffectiveDatee FROM M_MRPMaster where Item_id=ItemID
+				and EffectiveDate<=EffectiveDate1 and Party_id is null and PartyType_id = PartyType and IsDeleted=0 order by EffectiveDate desc, id desc limit 1;	
 			end if;
             if MRPP is null  then
-				SELECT id, MRP into IDD,MRPP FROM M_MRPMaster where Item_id=ItemID
-				and EffectiveDate<=EffectiveDate1 and Party_id is null and PartyType_id is null order by EffectiveDate desc, id desc limit 1;
+				SELECT id, MRP, EffectiveDate into IDD,MRPP,EffectiveDatee FROM M_MRPMaster where Item_id=ItemID
+				and EffectiveDate<=EffectiveDate1 and Party_id is null and PartyType_id is null and IsDeleted=0 order by EffectiveDate desc, id desc limit 1;
 			END IF;
         
         end if;    
        
-       
+        
         if Modee = 1 then
 			set result=IDD;
-        ELSE 
+        elseif Modee = 2 then
 			set result=MRPP;
+        elseif  Modee = 3 then  
+			set result = EffectiveDatee;
         END IF;
         
 RETURN result;
