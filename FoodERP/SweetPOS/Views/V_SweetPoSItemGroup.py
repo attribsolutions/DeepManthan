06eeb02630b7d6cad51fcf5b5f622ef0a1c394c2
pradnyaubale <pradnyaubale12@gmail.com>
2024-11-30 11:41:36 +0530

    Update V_SweetPoSItemGroup.py

--- FoodERP/SweetPOS/Views/V_SweetPoSItemGroup.py
+++ FoodERP/SweetPOS/Views/V_SweetPoSItemGroup.py
@@ -93,14 +93,14 @@ class ItemListView(CreateAPIView):
                 ifnull(i.BaseUnitID_id,0) AS UnitID, i.IsFranchisesItem,  
                 ifnull(Round(GetTodaysDateMRP(i.id, CURDATE(), 2, 0,0,0),2),0.0) AS FoodERPMRP,
                 ifnull(MC_ItemGroupDetails.SubGroup_id,0) AS ItemGroupID,MC_ItemGroupDetails.ItemSequence,
-                i.IsCBMItem ,i.IsMixItem
+                i.IsCBMItem ,i.IsMixItem,ifnull(SweetPOS.SPOSRateMaster(i.id),"0,0,0,0")OnlineRates
                 FROM M_Items AS i
                 left join MC_ItemGroupDetails on MC_ItemGroupDetails.Item_id=i.id and GroupType_id=5
                 LEFT JOIN M_ChannelWiseItems ON i.id = M_ChannelWiseItems.Item_id
                 join MC_PartyItems on MC_PartyItems.Item_id=i.id and MC_PartyItems.party_id={DivisionID}
                 WHERE M_ChannelWiseItems.PartyType_id = 19  """)                  
                 # return Response({"status": True, "status_code": 200, "count": "", "data": "" }, status=200)
-                
+                # print(query)
                 count=0
                 item_data=list()
                 for row in query:
@@ -114,14 +114,16 @@ class ItemListView(CreateAPIView):
                             "EffectiveFrom":row.RateEffectiveDate
                     })
 
-                    queryforRate =M_SPOSRateMaster.objects.filter(ItemID=row.id,IsDeleted=0)
-                    for RateRow in queryforRate:
-                        Ratelist.append({	
-                            "Rate": RateRow.Rate,
-                            "POSRateType": RateRow.POSRateType,
-                            "IsChangeRateToDefault": RateRow.IsChangeRateToDefault,
-                            "EffectiveFrom":RateRow.EffectiveFrom
-                        })
+                    # queryforRate =M_SPOSRateMaster.objects.filter(ItemID=row.id,IsDeleted=0)
+                    # for RateRow in queryforRate:
+
+                    OnlineRates=(row.OnlineRates).split(',') 
+                    Ratelist.append({	
+                        "Rate": float(OnlineRates[0]),
+                        "POSRateType": int(OnlineRates[1]),
+                        "IsChangeRateToDefault": bool(OnlineRates[2]),
+                        "EffectiveFrom":OnlineRates[3]
+                    })
                     
                     
                     item_data.append({
@@ -153,4 +155,4 @@ class ItemListView(CreateAPIView):
                 return Response({'status': False, 'status_code': 401, 'message': 'Unauthorized'}, status=401)
         except Exception as e:
             log_entry = create_transaction_logNew(request, 0, DivisionID,'ItemList:'+str(e),33,0)
-            return Response({'status': False, 'status_code': 400, 'message': str(e), 'data': []}, status=400)
+            return Response({'status': False, 'status_code': 400, 'message': Exception(e), 'data': []}, status=400)
