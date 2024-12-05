60c3b6b961acab69f396d32ebd99fa70247f3b26
shrutip45874 <159874290+shrutip45874@users.noreply.github.com>
2024-12-05 14:57:57 +0530

    common function page and add basic auth service settings api.
    common function page and add basic auth service settings api.

--- FoodERP/SweetPOS/Views/V_SPOSServicesSettings.py
+++ FoodERP/SweetPOS/Views/V_SPOSServicesSettings.py
@@ -1,37 +1,40 @@
-import base64
+
 from ..models import *
 from ..Serializer.S_SweetPosRoleAccess import *
 from django.http import JsonResponse
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from django.db import transaction
-import pdb
+from SweetPOS.Views.SweetPOSCommonFunction import BasicAuthenticationfunction
 from FoodERPApp.Views.V_CommFunction import create_transaction_logNew
 from FoodERPApp.models import *
 from ..Serializer.S_SPOSServicesSettings import *
+from rest_framework.authentication import BasicAuthentication
 
 
 class SweetPosServiceSettingsImportView(CreateAPIView):
-    permission_classes = (IsAuthenticated,)   
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = [BasicAuthentication]
 
     @transaction.atomic()
     def get(self, request,id=0):
         try:
-            with transaction.atomic():
+           
+            with transaction.atomic():                
+                user=BasicAuthenticationfunction(request)                    
+            if user is not None:
                 ServiceSettingsData = M_ServiceSettings.objects.raw('''Select 1 as id, Party PartyID,GeneralMaster.Name SettingName,ServiceSettingsID ,Flag,Value,Access 
-                                                                       from M_ServiceSettings 
+                                                                       from M_ServiceSettings
                                                                        JOIN FoodERP.M_GeneralMaster GeneralMaster ON GeneralMaster.id=ServiceSettingsID  
-                                                                       where Party=%s''',[id])   
-
-                # print(ServiceSettingsData.query)                    
+                                                                       where Party=%s''',[id])                                    
                 
                 ServicesSettings_serializer = SPOSServicesSettingstSerializer(ServiceSettingsData, many=True).data
                 
                 log_entry = create_transaction_logNew(request, ServicesSettings_serializer,0,'',347,0)
                 return JsonResponse({'StatusCode': 200, 'Status': True, 'Message':'','Data': ServicesSettings_serializer})
         except  M_SweetPOSRoleAccess.DoesNotExist:
-            log_entry = create_transaction_logNew(request, 0,0,'GETSweetPOSRoleAccessDetails'+'RoleAccess Not available',347,0)
-            return JsonResponse({'StatusCode': 204, 'Status': True,'Message':  'RoleAccess Not available', 'Data': []})
+            log_entry = create_transaction_logNew(request, 0,0,'GETSweetPOSServiceSettingDetails'+'Service Settings Not available',347,0)
+            return JsonResponse({'StatusCode': 204, 'Status': True,'Message':  'Service Settings Not available', 'Data': []})
         except Exception as e:
-            log_entry = create_transaction_logNew(request, 0,0,'GETSweetPOSRoleAccessDetails:'+str(e),33,0)
+            log_entry = create_transaction_logNew(request, 0,0,'GETSweetPOSServiceSettingDetails:'+str(e),33,0)
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message':  str(e), 'Data':[]})
