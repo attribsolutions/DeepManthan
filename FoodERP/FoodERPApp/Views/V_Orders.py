9b143cd0f87d7a7c721715b34d830ff0e13e8ae0
PratikshaYadav19 <pratikshayadav222.com>
2024-12-18 17:15:37 +0530

    Update V_Orders.py
    set BaseUnitConversion to UnitName

--- FoodERP/FoodERPApp/Views/V_Orders.py
+++ FoodERP/FoodERPApp/Views/V_Orders.py
@@ -522,6 +522,7 @@ class T_OrdersViewSecond(CreateAPIView):
                                 
                                 aaaa = UnitwiseQuantityConversion(
                                     b['Item']['id'], b['Quantity'], b['Unit']['id'], 0, 0, 0, 1).GetConvertingBaseUnitQtyBaseUnitName()
+                                print(aaaa)
                                 
                                 if (aaaa == b['Unit']['UnitID']['Name']):
                                     bb=''
@@ -557,7 +558,7 @@ class T_OrdersViewSecond(CreateAPIView):
                                     "Rate": b['Rate'],
                                     "Unit": b['Unit']['id'],
                                     "PrimaryUnitName":b['Unit']['UnitID']['Name'],
-                                    "UnitName": bb,
+                                    "UnitName": b['Unit']['BaseUnitConversion'],
                                     "SAPUnitName": b['Unit']['UnitID']['SAPUnit'],
                                     "BaseUnitQuantity": b['BaseUnitQuantity'],
                                     "GST": b['GST']['id'],
