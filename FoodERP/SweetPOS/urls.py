b5a0db626718555e154656e1957535ed6459245d
pradnyaubale <87567521+pradnyaubale@users.noreply.github.com>
2025-02-07 11:56:10 +0530

    SPOSStockProcessing thought cronjob

--- FoodERP/SweetPOS/urls.py
+++ FoodERP/SweetPOS/urls.py
@@ -46,6 +46,7 @@ urlpatterns = [
     url(r'SPOSUsersOfDivision/(?P<Division_id>\d+)$',SweetPOSUsersSecondView.as_view()),    
     url(r'sposroleslist$', SweetPOSRolesView.as_view()),
     url(r'StockProcesSPOS$',SPOSStockProcessingView.as_view()),
+    url(r'CronjobStockProcesSPOS$',SPOSStockProcessingthoughtcronjobView.as_view()),
     url(r'Invoice/(?P<id>\d+)(?:/(?P<characters>[A-Z]+))?$',SPOSInvoiceViewSecond.as_view()),
     url(r'CashierSummary$',SPOSCashierSummaryList.as_view()),
     url(r'UpdateCustomerVehiclePOSinvoice$',UpdateCustomerVehiclePOSInvoiceView.as_view()),
