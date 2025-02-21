e1d163602e94a0da170b8ebcc2f3c9411d48347f
shrutip45874 <159874290+shrutip45874@users.noreply.github.com>
2025-02-21 12:21:35 +0530

    Update V_Reports.py

--- FoodERP/FoodERPApp/Views/V_Reports.py
+++ FoodERP/FoodERPApp/Views/V_Reports.py
@@ -298,6 +298,8 @@ left JOIN FoodERP.T_Orders ON FoodERP.T_Orders.id = FoodERP.TC_InvoicesReference
                         # print(Final_Data)   
                     else:
                         # print("Ru")
+                        # MC_PartySubParty join use madhe nahi so remove kela aahe. 
+                        # JOIN FoodERP.MC_PartySubParty ON FoodERP.MC_PartySubParty.SubParty_id = SweetPOS.T_SPOSInvoices.Customer and FoodERP.MC_PartySubParty.Party_id=SweetPOS.T_SPOSInvoices.Party
                         query = '''SELECT SweetPOS.TC_SPOSInvoiceItems.ClientID,SweetPOS.TC_SPOSInvoiceItems.id, A.SAPPartyCode SAPPartyID, SweetPOS.T_SPOSInvoices.Party AS PartyID,A.Name PartyName, X.Name PartyType, '' ImportFromExcel,  SweetPOS.T_SPOSInvoices.FullInvoiceNumber,
 SweetPOS.T_SPOSInvoices.InvoiceDate,SweetPOS.T_SPOSInvoices.Customer AS CustomerID,B.Name CustomerName, Y.Name CustomeType, M_Drivers.Name DriverName,
 M_Vehicles.VehicleNumber VehicleNo,SweetPOS.TC_SPOSInvoiceItems.Item AS ItemID,M_Items.Name ItemName,C_Companies.Name CompanyName,
@@ -312,7 +314,6 @@ SweetPOS.T_SPOSInvoices.RoundOffAmount,SweetPOS.T_SPOSInvoices.GrandTotal,M_Grou
 M_Cluster.Name AS Cluster, M_SubCluster.Name AS SubCluster, SweetPOS.TC_SPOSInvoiceItems.BatchCode AS BatchNo , SweetPOS.TC_SPOSInvoiceItems.BatchDate, M_Items.SAPItemCode SAPItemID
 FROM SweetPOS.TC_SPOSInvoiceItems
 JOIN SweetPOS.T_SPOSInvoices ON SweetPOS.T_SPOSInvoices.id = SweetPOS.TC_SPOSInvoiceItems.Invoice_id
-
 left JOIN SweetPOS.TC_SPOSInvoicesReferences ON SweetPOS.TC_SPOSInvoicesReferences.Invoice_id = SweetPOS.T_SPOSInvoices.id
 left JOIN FoodERP.T_Orders ON FoodERP.T_Orders.id = SweetPOS.TC_SPOSInvoicesReferences.Order
  JOIN FoodERP.M_Parties A ON A.id = SweetPOS.T_SPOSInvoices.Party
@@ -334,7 +335,7 @@ left JOIN FoodERP.T_Orders ON FoodERP.T_Orders.id = SweetPOS.TC_SPOSInvoicesRefe
  WHERE SweetPOS.T_SPOSInvoices.InvoiceDate BETWEEN %s AND %s AND SweetPOS.T_SPOSInvoices.Party = %s'''
                 Franchises_Data = list(T_SPOSInvoices.objects.raw(query, [FromDate, ToDate, party_id]))  
                 Final_Data.extend(Franchises_Data)   
-                # print(query)
+                # print(Franchises_Data)
                 # Genericdataquery = Final_Data    
                 # print(Genericdataquery)         
                 if Final_Data:
