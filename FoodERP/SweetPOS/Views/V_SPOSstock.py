1e254f221cbd8cefa6eec06cfb2cb1e7c28c8f1e
shrutip45874 <159874290+shrutip45874@users.noreply.github.com>
2025-03-04 12:58:29 +0530

    Update V_SPOSstock.py

--- FoodERP/SweetPOS/Views/V_SPOSstock.py
+++ FoodERP/SweetPOS/Views/V_SPOSstock.py
@@ -185,12 +185,18 @@ class SPOSStockReportView(CreateAPIView):
                         "PartyName": PartyNameQ[0]["Name"],
                         "StockDetails": serializer})
                     # print(StockreportQuery)
+                # if StockData:
+                #     log_entry = create_transaction_logNew(request, Orderdata, 0, 'From:'+str(FromDate)+','+'To:'+str(ToDate), 210, 0, FromDate, ToDate, 0)
+                #     return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': '', 'Data': StockData})
+                # else:
+                #     log_entry = create_transaction_logNew(request, Orderdata, 0, 'Recort Not Found', 210, 0)
+                #     return JsonResponse({'StatusCode': 204, 'Status': True, 'Message': 'Record Not Found', 'Data': []})
+                
                 if StockData:
-                    log_entry = create_transaction_logNew(request, Orderdata, 0, 'From:'+str(FromDate)+','+'To:'+str(ToDate), 210, 0, FromDate, ToDate, 0)
                     return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': '', 'Data': StockData})
                 else:
-                    log_entry = create_transaction_logNew(request, Orderdata, 0, 'Recort Not Found', 210, 0)
                     return JsonResponse({'StatusCode': 204, 'Status': True, 'Message': 'Record Not Found', 'Data': []})
+    
         except Exception as e:
             log_entry = create_transaction_logNew(request,Orderdata, 0, 'StockReport:'+str(e), 33, 0)
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message': str(e), 'Data': []})        
