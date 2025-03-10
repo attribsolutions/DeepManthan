914c03f450098ebd4128d4327f97b2a2773621e9
PratikshaYadav19 <pratikshayadav222.com>
2025-03-10 16:10:25 +0530

    Update V_Scheme.py
    Add MC_SchemeItems condition

--- FoodERP/FoodERPApp/Views/V_Scheme.py
+++ FoodERP/FoodERPApp/Views/V_Scheme.py
@@ -6,64 +6,77 @@ from ..Views.V_CommFunction import *
 from ..models import *
 
 
+     
 class SchemeView(CreateAPIView):
     permission_classes = (IsAuthenticated,)
-    authentication_classes = [BasicAuthentication]    
+    authentication_classes = [BasicAuthentication]
+
     @transaction.atomic()
     def post(self, request):
         PartyData = JSONParser().parse(request)
         try:
             with transaction.atomic():
-                user=BasicAuthenticationfunction(request)
-                
+                user = BasicAuthenticationfunction(request)
                 Party = PartyData['Party']
+                
                 if user is not None:
-                    SchemeDetails = M_Scheme.objects.raw(f'''select M_Scheme.id,SchemeName,SchemeValue,ValueIn,FromPeriod,ToPeriod,FreeItemID,VoucherLimit,QrPrefix,SchemeTypeName,SchemeTypeID_id,UsageTime,
-                    BillAbove,UsageType,BillEffect,IsQrApplicable,IsActive from M_Scheme 
-                    JOIN M_SchemeType ON M_Scheme.SchemeTypeID_id=M_SchemeType.id 
-                    JOIN MC_SchemeParties ON MC_SchemeParties.SchemeID_id=M_Scheme.id
-                    Where PartyID_id={Party} and IsActive=1''');
+                    SchemeDetails = M_Scheme.objects.raw(f'''
+                        SELECT M_Scheme.id, SchemeName, SchemeValue, ValueIn, FromPeriod, ToPeriod, FreeItemID, VoucherLimit,
+                        QrPrefix, SchemeTypeName, SchemeTypeID_id, UsageTime, BillAbove, UsageType, BillEffect,
+                        IsQrApplicable, IsActive 
+                        FROM M_Scheme 
+                        JOIN M_SchemeType ON M_Scheme.SchemeTypeID_id = M_SchemeType.id 
+                        JOIN MC_SchemeParties ON MC_SchemeParties.SchemeID_id = M_Scheme.id
+                        WHERE PartyID_id = {Party} AND IsActive = 1
+                    ''')
+                    
                     data = []
                     for Scheme in SchemeDetails:   
-                        
                         qr_codes = M_SchemeQRs.objects.filter(SchemeID_id=Scheme.id).values('id', 'QRCode')
-                        qr_list = []
-                        for qr in qr_codes:           
-                            qr_list.append({"QRID": qr['id'], "QRCode": qr['QRCode']})
-                        scheme_data={
-                                "SchemeId": Scheme.id,
-                                "SchemeName":Scheme.SchemeName,
-                                "SchemeValue":Scheme.SchemeValue,
-                                "ValueIn":Scheme.ValueIn,
-                                "FromPeriod":Scheme.FromPeriod,
-                                "ToPeriod":Scheme.ToPeriod,
-                                "FreeItemID":Scheme.FreeItemID,
-                                "VoucherLimit":Scheme.VoucherLimit,
-                                "BillAbove":Scheme.BillAbove,
-                                "QrPrefix":Scheme.QrPrefix,
-                                "IsActive":Scheme.IsActive,
-                                "SchemeTypeID_id":Scheme.SchemeTypeID_id,
-                                "SchemeTypeName":Scheme.SchemeTypeName,
-                                "UsageTime":Scheme.UsageTime,
-                                "UsageType":Scheme.UsageType,
-                                "BillEffect":Scheme.BillEffect,                                     
-                                "IsQrApplicable":Scheme.IsQrApplicable,
-                                'QR_Codes': qr_list    
-                            }
+                        qr_list = [{"QRID": qr['id'], "QRCode": qr['QRCode']} for qr in qr_codes]
+
+                        SchemeItems = MC_SchemeItems.objects.filter(SchemeID=Scheme.id).values('TypeForItem', 'Item')
+                  
+                        ItemsType = {1: [], 2: [], 3: []}
+                        for Item in SchemeItems:
+                            ItemsType[Item['TypeForItem']].append(str(Item['Item']))
                             
+                        applicable_items = ",".join(ItemsType[1]) if ItemsType[1] else None
+                        non_applicable_items = ",".join(ItemsType[2]) if ItemsType[2] else None
+                        effective_items = ",".join(ItemsType[3]) if ItemsType[3] else None
+
+                        scheme_data = {
+                            "SchemeId": Scheme.id,
+                            "SchemeName": Scheme.SchemeName,
+                            "SchemeValue": Scheme.SchemeValue,
+                            "ValueIn": Scheme.ValueIn,
+                            "FromPeriod": Scheme.FromPeriod,
+                            "ToPeriod": Scheme.ToPeriod,
+                            "FreeItemID": Scheme.FreeItemID,
+                            "VoucherLimit": Scheme.VoucherLimit,
+                            "BillAbove": Scheme.BillAbove,
+                            "QrPrefix": Scheme.QrPrefix,
+                            "IsActive": Scheme.IsActive,
+                            "SchemeTypeID_id": Scheme.SchemeTypeID_id,
+                            "SchemeTypeName": Scheme.SchemeTypeName,
+                            "UsageTime": Scheme.UsageTime,
+                            "UsageType": Scheme.UsageType,
+                            "BillEffect": Scheme.BillEffect,                                     
+                            "IsQrApplicable": Scheme.IsQrApplicable,
+                            "QR_Codes": qr_list,
+                            "ApplicableItems": applicable_items,
+                            "NonApplicableItems": non_applicable_items,
+                            "EffectiveItems": effective_items
+                        }
                         data.append(scheme_data)
-                        
-                if data:                    
-                    log_entry = create_transaction_logNew(request, PartyData, 0, 'SchemeDetails:' + str(data), 433, 0)
+
+                if data:
+                    create_transaction_logNew(request, PartyData, 0, 'SchemeDetails:' + str(data), 433, 0)
                     return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': '', 'Data': data})
                 else:
-                    log_entry = create_transaction_logNew(request, PartyData, Party, 'Record Not Found', 433, 0)
+                    create_transaction_logNew(request, PartyData, Party, 'Record Not Found', 433, 0)
                     return JsonResponse({'StatusCode': 204, 'Status': True, 'Message': 'Record Not Found', 'Data': []})
+
         except Exception as e:
-                    log_entry = create_transaction_logNew(request, PartyData, 0, 'SchemeDetails:' + str(e), 33, 0)
-                    return JsonResponse({'StatusCode': 400, 'Status': True, 'Message': str(e), 'Data': []})       
-    
-                
-    
-               
-     
+            create_transaction_logNew(request, PartyData, 0, 'SchemeDetails:' + str(e), 33, 0)
+            return JsonResponse({'StatusCode': 400, 'Status': True, 'Message': str(e), 'Data': []})
