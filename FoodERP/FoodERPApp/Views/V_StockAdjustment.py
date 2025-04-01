2b0270795be4c4e02c98d5e942a8621fc7f52cf2
pradnyaubale <pradnyaubale12@gmail.com>
2025-04-01 09:27:42 +0530

    Update V_StockAdjustment.py

--- FoodERP/FoodERPApp/Views/V_StockAdjustment.py
+++ FoodERP/FoodERPApp/Views/V_StockAdjustment.py
@@ -177,7 +177,7 @@ class CheckStockEntryDateAndNotAllowedBackdatedTransactionView(CreateAPIView):
                 return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': '', 'Data': bool(result)})
             else:  
                 log_entry = create_transaction_logNew(request,TransactionData, PartyID,f'Backdated transactions not allowed for Party: {PartyID} Date: {TransactionDate}',360,0)
-                return JsonResponse({'StatusCode': 400, 'Status': True, 'Message': 'Backdated transactions not allowed', 'Data': bool(result)})
+                return JsonResponse({'StatusCode': 200, 'Status': True, 'Message': 'Backdated transactions not allowed', 'Data': bool(result)})
         except Exception as e:
             log_entry = create_transaction_logNew(request,0, 0,'TransactionData:'+str(Exception(e)),33,0)
             return JsonResponse({'StatusCode': 400, 'Status': True, 'Message': str(e), 'Data': []})
