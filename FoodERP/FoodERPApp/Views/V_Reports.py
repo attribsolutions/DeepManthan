158605c529d890c33a5b06c1c555c043b83e0fa6
shrutip45874 <159874290+shrutip45874@users.noreply.github.com>
2025-06-06 11:58:50 +0530

    Update V_Reports.py

--- FoodERP/FoodERPApp/Views/V_Reports.py
+++ FoodERP/FoodERPApp/Views/V_Reports.py
@@ -1851,7 +1851,7 @@ where  M_Parties.id=%s or MC_PartySubParty.Party_id=%s and M_PriceList.id in (%s
                             "FE2ItemID": row.id,
                             "SAPCode": row.SAPItemCode,
                             "Barcode": row.BarCode,
-                            "HSNCode": ''+ row.HSNCode +'',
+                            "HSNCode": row.HSNCode,
                             "Company": row.CompanyName,
                             "SKUStatus(T,F)": row.isActive,
                             "BoxSize": row.BoxSize,
