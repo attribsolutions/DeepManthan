51a06a60a7ab7eb8e5b2a875ff0fae47418a1fd5
attribsolutions <40659065+attribsolutions@users.noreply.github.com>
2025-06-26 11:25:56 +0530

    pos setting api changes

--- FoodERP/SweetPOS/urls.py
+++ FoodERP/SweetPOS/urls.py
@@ -63,8 +63,8 @@ urlpatterns = [
     url(r'SweetPosServiceSettingsImport/([0-9]+)$', SweetPosServiceSettingsImportView.as_view()),
     
     #Pos Settings
-    url(r'PosSettings$',PosSettings.as_view()),
-    url(r'PosSettings/(?P<pk>[0-9]+)/$', PosSettings.as_view()), 
+    url(r'^PosSettings$', PosSettings.as_view()), 
+    url(r'^PosSettings/(?P<pk>[0-9]+)/$', PosSettingsDetail.as_view()),
     url(r'^paymentmodes/(?P<party_id>[0-9]+)/$', PaymentModeAPIView.as_view(), name='payment-modes'),
  
     ]
